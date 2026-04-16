4b81a3b3b8e64d207f2f2c4b47112adf559f45c1
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-04-16 13:52:20 -0400

    refactor: add GTK3 file chooser button stub file

--- src/pytkwrap/gtk3/buttons/file_chooser_button.pyi
+++ src/pytkwrap/gtk3/buttons/file_chooser_button.pyi
@@ -1,10 +1,18 @@
-# RAMSTK Package Imports
-from ramstk.views.gtk3 import Gtk as Gtk
+# Third Party Imports
+from _typeshed import Incomplete
 
-# RAMSTK Local Imports
-from ..widget import WidgetProperties as WidgetProperties
-from . import RAMSTKButton as RAMSTKButton
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.buttons import GTK3BaseButton as GTK3BaseButton
+from pytkwrap.gtk3.widget import GTK3WidgetProperties as GTK3WidgetProperties
 
-class RAMSTKFileChooserButton(Gtk.FileChooserButton, RAMSTKButton):
+class GTK3FileChooserButton(Gtk.FileChooserButton, GTK3BaseButton):
+    _DEFAULT_EDIT_SIGNAL: str
+    _DEFAULT_HEIGHT: int
+    _DEFAULT_WIDTH: int
+    _GTK3_FILE_CHOOSER_BUTTON_PROPERTIES: Incomplete
+    _GTK3_FILE_CHOOSER_BUTTON_SIGNALS: Incomplete
     def __init__(self, label: str = "...") -> None: ...
-    def do_set_properties(self, properties: WidgetProperties) -> None: ...
+    def do_set_properties(self, properties: GTK3WidgetProperties) -> None: ...
+    def do_get_value(self) -> str | None: ...
+    def do_set_value(self, value: str | None) -> None: ...
