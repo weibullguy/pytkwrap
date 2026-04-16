856a0cce87eb14f14cc5e111f931de6d1f2d2c01
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-04-16 08:16:18 -0400

    refactor: add GTK3 base button stub file

--- src/pytkwrap/gtk3/buttons/base_button.pyi
+++ src/pytkwrap/gtk3/buttons/base_button.pyi
@@ -1,14 +1,24 @@
+# Standard Library Imports
+from collections.abc import Callable as Callable
+from typing import Any
+
+# Third Party Imports
 from _typeshed import Incomplete
-from pytkwrap.gtk3._libs import GdkPixbuf as GdkPixbuf, Gtk as Gtk
-from pytkwrap.gtk3.widget import BaseWidget as BaseWidget, WidgetProperties as WidgetProperties
-from typing import Any, Callable
 
-def do_make_buttonbox(icons: list[str], tooltips: list[str], callbacks: list[Callable], **kwargs: Any) -> Gtk.HButtonBox | Gtk.VButtonBox: ...
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import GdkPixbuf as GdkPixbuf
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.widget import GTK3BaseWidget as GTK3BaseWidget
+from pytkwrap.gtk3.widget import GTK3WidgetProperties as GTK3WidgetProperties
 
-class BaseButton(Gtk.Button, BaseWidget):
+def do_make_buttonbox(
+    icons: list[str], tooltips: list[str], callbacks: list[Callable], **kwargs: Any
+) -> Gtk.HButtonBox | Gtk.VButtonBox: ...
+
+class GTK3BaseButton(Gtk.Button, GTK3BaseWidget):
     _BUTTON_PROPERTIES: Incomplete
     _BUTTON_SIGNALS: Incomplete
     _DEFAULT_HEIGHT: int
     _DEFAULT_WIDTH: int
-    def __init__(self, label: str = '...') -> None: ...
-    def do_set_properties(self, properties: WidgetProperties) -> None: ...
+    def __init__(self, label: str = "...") -> None: ...
+    def do_set_properties(self, properties: GTK3WidgetProperties) -> None: ...
