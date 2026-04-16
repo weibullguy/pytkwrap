e9c549f9fa645f5df849beaa78601da257158ce2
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-04-16 08:54:17 -0400

    refactor: add GTK3 check button stub file

--- src/pytkwrap/gtk3/buttons/check_button.pyi
+++ src/pytkwrap/gtk3/buttons/check_button.pyi
@@ -1,16 +1,20 @@
-# Standard Library Imports
-from datetime import date
+# Third Party Imports
+from _typeshed import Incomplete
 
-# RAMSTK Package Imports
-from ramstk.views.gtk3 import Gtk as Gtk
-from ramstk.views.gtk3.widgets import WidgetProperties as WidgetProperties
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.buttons import GTK3BaseButton as GTK3BaseButton
+from pytkwrap.gtk3.widget import GTK3BaseDataWidget as GTK3BaseDataWidget
+from pytkwrap.gtk3.widget import GTK3WidgetProperties as GTK3WidgetProperties
 
-# RAMSTK Local Imports
-from . import RAMSTKButton as RAMSTKButton
-
-class RAMSTKCheckButton(Gtk.CheckButton, RAMSTKButton):
+class GTK3CheckButton(Gtk.CheckButton, GTK3BaseButton, GTK3BaseDataWidget):
+    _CHECK_BUTTON_PROPERTIES: Incomplete
+    _CHECK_BUTTON_SIGNALS: Incomplete
+    _DEFAULT_EDIT_SIGNAL: str
+    _DEFAULT_HEIGHT: int
+    _DEFAULT_WIDTH: int
+    default: bool
     def __init__(self, label: str = "") -> None: ...
-    def do_set_properties(self, properties: WidgetProperties) -> None: ...
-    def do_update(
-        self, package: dict[str, bool | date | float | int | str]
-    ) -> None: ...
+    def do_set_properties(self, properties: GTK3WidgetProperties) -> None: ...
+    def do_get_value(self) -> bool: ...
+    def do_set_value(self, value: bool | int) -> None: ...
