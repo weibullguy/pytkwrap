e2f497bd613593d63f8bfefb6303f8ab94284b06
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-04-16 08:17:48 -0400

    refactor: update GTK3 check button with new mixins

--- src/pytkwrap/gtk3/buttons/check_button.py
+++ src/pytkwrap/gtk3/buttons/check_button.py
@@ -1,26 +1,26 @@
-# -*- coding: utf-8 -*-
-#
 #       pytkwrap.gtk3.buttons.check_button.py is part of the pytkwrap project
 #
 # All rights reserved.
 # Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
-"""The pytkwrap GTK3 CheckButton module."""
-
-# Standard Library Imports
-from typing import Dict
-
-# Third Party Imports
-from pubsub import pub
+"""The pytkwrap GTK3CheckButton module."""
 
 # pytkwrap Package Imports
-from pytkwrap.exceptions import UnkSignalError
-from pytkwrap.gtk3.widget import GTK3BaseWidget, GTK3WidgetProperties
 from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.buttons import GTK3BaseButton
+from pytkwrap.gtk3.widget import (
+    GTK3BaseDataWidget,
+    GTK3WidgetProperties,
+)
 
 
-class GTK3CheckButton(Gtk.CheckButton, GTK3BaseButton):
-    """The CheckButton class."""
+# pylint: disable=too-many-ancestors
+# GTK3's own inheritance chain accounts for 7 of the 9 ancestors.
+# The pytkwrap additions (GTK3BaseButton, GTK3BaseDataWidget) are necessary
+# to provide button label handling and pubsub data widget support.
+# TODO: Refactor the GTKBaseButton to inherit from GTK3BaseDataWidget instead of
+#  GTK3BaseWidget and then remove the GTK3BaseDataWidget ancestor from here.
+class GTK3CheckButton(Gtk.CheckButton, GTK3BaseButton, GTK3BaseDataWidget):
+    """The GTK3CheckButton class."""
 
     # Define private class attributes.
     _CHECK_BUTTON_PROPERTIES = GTK3WidgetProperties(
@@ -34,28 +34,33 @@ class GTK3CheckButton(Gtk.CheckButton, GTK3BaseButton):
     _DEFAULT_WIDTH = 200
 
     def __init__(self, label: str = "") -> None:
-        """Initialize an instance of the CheckButton widget.
+        """Initialize an instance of the GTK3CheckButton widget.
 
-        :param label: the text to display with the Gtk.CheckButton. Default is an empty
-            string.
+        Parameters
+        ----------
+        label : str
+            The text to display with the GTK3CheckButton. Default is an empty string.
         """
         Gtk.CheckButton.__init__(self)
+        GTK3BaseDataWidget.__init__(self)
         GTK3BaseButton.__init__(self, label=label)
 
         # Initialize public instance attributes.
         self.dic_properties.update(self._CHECK_BUTTON_PROPERTIES)
-        self.dic_handler_id.update(
-            {_signal: -1 for _signal in self._CHECK_BUTTON_SIGNALS}
-        )
+        self.dic_handler_id.update({
+            _signal: -1 for _signal in self._CHECK_BUTTON_SIGNALS
+        })
 
         self.default = False
 
     # ----- ----- Standard widget methods. ----- ----- #
     def do_set_properties(self, properties: GTK3WidgetProperties) -> None:
-        """Set the properties of the CheckButton.
+        """Set the properties of the GTK3CheckButton.
 
-        :param properties: the WidgetProperties dict with the property values to set for
-            the CheckButton.
+        Parameters
+        ----------
+        properties : GTK3WidgetProperties
+            The typed dict with the property values to set for the GTK3CheckButton.
         """
         super().do_set_properties(properties)
 
@@ -73,60 +78,26 @@ class GTK3CheckButton(Gtk.CheckButton, GTK3BaseButton):
                 _property.replace("_", "-"), self.dic_properties[_property]
             )
 
-    def do_update(self, package: Dict[str, bool]) -> None:
-        """Update the CheckButton with a new value.
+    # ----- ----- Check Button specific methods. ----- ----- #
+    def do_get_value(self) -> bool:  # type: ignore[override]
+        """Retrieve the status of the GTK3CheckButton.
 
-        :param package: the date package to use to update the CheckButton.
+        Returns
+        -------
+        bool
+            The status of the GTK3CheckButton.
         """
-        _field, _value = next(iter(package.items()))
+        return self.get_active()
 
-        if _field != self.field:
-            return
+    def do_set_value(self, value: bool | int) -> None:
+        """Set the GTK3CheckButton active value.
 
-        if _value is None:
-            self.set_active(False)
-            return
-
-        try:
-            _hid = self.dic_handler_id.get(self.edit_signal, -1)
-            with self.handler_block(_hid):
-                self.set_active(_value)
-        except (KeyError, OverflowError) as exc:
-            _error_msg = self.dic_error_message["unk_signal"].format(
-                "CheckButton.do_update()",
-                self.edit_signal,
-            )
-            pub.sendMessage(
-                "do_log_error",
-                message=_error_msg,
-            )
-            raise UnkSignalError(_error_msg) from exc
-
-    def on_changed(self, __checkbutton: GTK3BaseWidget) -> None:
-        """Retrieve the data package for the CheckButton on value changes.
-
-        This method also sends a PyPubSub message along with the data package for
-        listeners to update with the new value.
-
-        :param __checkbutton: the CheckButton whose signal called this method. Unused
-            but required to satisfy the Gtk.CheckButton() callback method structure.
+        Parameters
+        ----------
+        value : bool or int or None
+            The desired status of the GTK3CheckButton.
         """
-        _package = {self.field: self.get_active()}
-        try:
-            _hid = self.dic_handler_id[self.edit_signal]
-            with self.handler_block(_hid):
-                pub.sendMessage(
-                    self.send_topic,
-                    node_id=self.record_id,
-                    package=_package,
-                )
-        except KeyError as exc:
-            _error_msg = self.dic_error_message["unk_signal"].format(
-                "CheckButton.on_changed()",
-                self.edit_signal,
-            )
-            pub.sendMessage(
-                "do_log_error",
-                message=_error_msg,
-            )
-            raise UnkSignalError(_error_msg) from exc
+        if value is None:
+            value = False
+
+        self.set_active(value)
