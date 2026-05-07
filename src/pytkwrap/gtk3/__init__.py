a83ef7f7355816ad6546ab9ed8604e4492a7c023
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-05-07 16:00:33 -0400

    feat: add GTK3Adjustment wrapper

--- src/pytkwrap/gtk3/__init__.py
+++ src/pytkwrap/gtk3/__init__.py
@@ -1,66 +1,70 @@
-#       pytkwrap.gtk3.__init__.py is part of the pytkwrap project
-#
-# All rights reserved.
-# Copyright since 2007 Doyle "weibullguy" Rowland doyle.rowland <AT> reliaqual <DOT> com
 """The pytkwrap GTK3 package.
 
 NOTE: Sub-modules in this package must import siblings directly
 (e.g. from pytkwrap.gtk3.combo import GTK3ComboBox)
 and never through this __init__.py to avoid circular imports.
+
+.. author:: Doyle Rowland
+.. copyright:: Since 2007, all rights reserved.
 """
 
 # pytkwrap Local Imports
-from .buttons import (
-    GTK3BaseButton,
-    GTK3CheckButton,
-    GTK3ColorButton,
-    GTK3FileChooserButton,
-    GTK3OptionButton,
-    GTK3SpinButton,
-    do_make_buttonbox,
-)
-from .combo import GTK3ComboBox
-from .entry import GTK3Entry
-from .frame import GTK3Frame
-from .label import (
-    GTK3Label,
-    do_make_label_group,
-)
-from .matrixview import GTK3MatrixView
+from .adjustment import GTK3Adjustment
+
+# from .buttons import (
+#    GTK3BaseButton,
+#    GTK3CheckButton,
+#    GTK3ColorButton,
+#    GTK3FileChooserButton,
+#    GTK3OptionButton,
+#    GTK3SpinButton,
+#    do_make_buttonbox,
+# )
+# from .cellrenderer import GTK3BaseCellRenderer
+# from .combo import GTK3ComboBox
+# from .entry import GTK3Entry
+# from .frame import GTK3Frame
+# from .label import (
+#    GTK3Label,
+#    do_make_label_group,
+# )
+# from .matrixview import GTK3MatrixView
 from .mixins import (
-    GTK3DataWidgetAttributes,
-    GTK3DataWidgetMixin,
-)
-from .plotview import GTK3PlotView
-from .scrolledwindow import GTK3ScrolledWindow
-from .textview import GTK3TextView
-from .widget import (
-    GTK3BaseDataWidget,
-    GTK3BaseWidget,
+    GTK3GObjectMixin,
+    GTK3WidgetAttributes,
     GTK3WidgetProperties,
+    set_widget_sensitivity,
 )
 
+# from .plotview import GTK3PlotView
+# from .scrolledwindow import GTK3ScrolledWindow
+# from .textview import GTK3TextView
+from .widget import GTK3Widget
 
 __all__ = [
-    "GTK3BaseButton",
-    "GTK3BaseDataWidget",
-    "GTK3BaseWidget",
-    "GTK3CheckButton",
-    "GTK3ColorButton",
-    "GTK3ComboBox",
-    "GTK3DataWidgetAttributes",
-    "GTK3DataWidgetMixin",
-    "GTK3Entry",
-    "GTK3FileChooserButton",
-    "GTK3Frame",
-    "GTK3Label",
-    "GTK3MatrixView",
-    "GTK3OptionButton",
-    "GTK3PlotView",
-    "GTK3ScrolledWindow",
-    "GTK3SpinButton",
-    "GTK3TextView",
+    #    "GTK3BaseButton",
+    #    "GTK3BaseDataWidget",
+    #    "GTK3BaseWidget",
+    #    "GTK3BaseCellRenderer",
+    #    "GTK3CheckButton",
+    #    "GTK3ColorButton",
+    #    "GTK3ComboBox",
+    #    "GTK3Entry",
+    #    "GTK3FileChooserButton",
+    #    "GTK3Frame",
+    "GTK3GObjectMixin",
+    #    "GTK3Label",
+    #    "GTK3MatrixView",
+    #    "GTK3OptionButton",
+    #    "GTK3PlotView",
+    #   "GTK3ScrolledWindow",
+    #    "GTK3SpinButton",
+    #    "GTK3TextView",
+    "GTK3Widget",
+    "GTK3WidgetAttributes",
     "GTK3WidgetProperties",
-    "do_make_buttonbox",
-    "do_make_label_group",
+    #    "do_make_buttonbox",
+    #    "do_make_label_group",
+    "set_widget_sensitivity",
+    "GTK3Adjustment",
 ]
---
+++ src/pytkwrap/gtk3/adjustment.py
@@ -0,0 +1,122 @@
+"""The pytkwrap GTK3 Adjustment module.
+
+.. author:: Doyle Rowland
+.. copyright:: Since 2007, all rights reserved.
+"""
+
+# Standard Library Imports
+from datetime import date
+
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk
+from pytkwrap.gtk3.mixins import GTK3GObjectMixin, GTK3WidgetProperties
+
+
+class GTK3Adjustment(Gtk.Adjustment, GTK3GObjectMixin):
+    """Wrapper for Gtk.Adjustment."""
+
+    _GTK3_ADJUSTMENT_PROPERTIES = GTK3WidgetProperties(
+        lower=0.0,
+        upper=0.0,
+        value=0.0,
+        step_increment=0.0,
+        page_increment=0.0,
+        page_size=0.0,
+    )
+    _GTK3_ADJUSTMENT_SIGNALS = [
+        "changed",
+        "notify",
+        "value-changed",
+    ]
+
+    def __init__(
+        self,
+        value: float = 0.0,
+        lower: float = 0.0,
+        upper: float = 0.0,
+        step_increment: float = 0.0,
+        page_increment: float = 0.0,
+        page_size: float = 0.0,
+    ) -> None:
+        Gtk.Adjustment.__init__(self)
+        GTK3GObjectMixin.__init__(self)
+
+        self.dic_properties.update(self._GTK3_ADJUSTMENT_PROPERTIES)
+        self.dic_handler_id = {_signal: -1 for _signal in self._GTK3_ADJUSTMENT_SIGNALS}
+
+        self.do_set_properties(
+            GTK3WidgetProperties(
+                lower=lower,
+                upper=upper,
+                page_increment=page_increment,
+                page_size=page_size,
+                step_increment=step_increment,
+                value=value,
+            )
+        )
+        self.do_set_value(self.dic_properties["value"])
+
+    def do_get_property(
+        self, property_name: str
+    ) -> bool | date | float | int | object | str | None:
+        """Get the value of the requested property.
+
+        Parameters
+        ----------
+        property_name : str
+            The name of the property to retrieve.
+
+        Returns
+        -------
+        bool | date | float | int | object | str | None
+        """
+        if property_name in self._GTK3_ADJUSTMENT_PROPERTIES:
+            return self.dic_properties.get(property_name)
+        return super().do_get_property(property_name)
+
+    def do_set_properties(
+        self,
+        properties: GTK3WidgetProperties | dict | list[list | tuple],
+    ) -> None:
+        """Set the properties of the GTK3Adjustment.
+
+        Parameters
+        ----------
+        properties : GTK3WidgetProperties | dict | list[list | tuple]
+            The typed dict, preferred, non-typed dict, list of lists, or list of tuples
+            with the property values to set for the GTK3Adjustment.
+        """
+        # Update the property dictionary.
+        self.dic_properties |= properties
+
+        self.set_lower(self.dic_properties["lower"])
+        self.set_page_increment(self.dic_properties["page_increment"])
+        self.set_page_size(self.dic_properties["page_size"])
+        self.set_step_increment(self.dic_properties["step_increment"])
+        self.set_upper(self.dic_properties["upper"])
+
+    def do_get_value(self) -> float:
+        """Return the current value of the GTK3Adjustment.
+
+        Returns
+        -------
+        float
+            The current value of the GTK3Adjustment.
+        """
+        return self.get_value()
+
+    def do_set_value(
+        self,
+        value: bool | date | float | int | str | None,
+    ) -> None:
+        """Set the current value of the GTK3Adjustment.
+
+        Parameters
+        ----------
+        value : bool | date | float | int | str | None
+            The value to set for the GTK3Adjustment.
+        """
+        if value is None or isinstance(value, date):
+            super().do_set_value(value)
+            return
+        self.set_value(float(value))
