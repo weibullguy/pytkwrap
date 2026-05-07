78bda634d6aa4b4ad83c4f616bfa1c4c640c516c
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-05-07 13:58:56 -0400

    refactor: update GTK3Widget stub file

--- src/pytkwrap/gtk3/widget.pyi
+++ src/pytkwrap/gtk3/widget.pyi
@@ -1,6 +1,4 @@
 # Standard Library Imports
-import abc
-from abc import abstractmethod
 from datetime import date
 
 # Third Party Imports
@@ -14,7 +12,7 @@ from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 from pytkwrap.utilities import none_to_default as none_to_default
 
-class GTK3Widget(Gtk.Widget, GTK3GObjectMixin, PyTkWrapMixin, metaclass=abc.ABCMeta):
+class GTK3Widget(Gtk.Widget, GTK3GObjectMixin, PyTkWrapMixin):
     _GTK3_WIDGET_PROPERTIES: Incomplete
     _GTK3_WIDGET_SIGNALS: Incomplete
     dic_properties: Incomplete
@@ -23,10 +21,6 @@ class GTK3Widget(Gtk.Widget, GTK3GObjectMixin, PyTkWrapMixin, metaclass=abc.ABCM
         self, property_name: str
     ) -> bool | date | float | int | object | str | None: ...
     def do_set_properties(self, properties: dict | list[list | tuple]) -> None: ...
-    @abstractmethod
-    def do_get_value(self) -> bool | date | float | int | str | None: ...
-    @abstractmethod
-    def do_set_value(self, value: bool | date | float | int | str | None) -> None: ...
     def do_update(
         self, package: dict[str, bool | date | float | int | str | None]
     ) -> None: ...
