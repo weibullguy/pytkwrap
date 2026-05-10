344a4eb45c4d648665906bf87b2ebb15f62b9367
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-05-10 13:10:21 -0400

    test: add GTK3FontButton test file

--- tests/gtk3/test_font_button.py
+++ tests/gtk3/test_font_button.py
@@ -1,4 +1,4 @@
-"""Test module for the GTK3ColorButton class.
+"""Test module for the GTK3FontButton class.
 
 .. author:: Doyle Rowland
 .. copyright:: Since 2007, all rights reserved.
@@ -11,7 +11,7 @@ from pubsub import pub
 # pytkwrap Package Imports
 from pytkwrap.exceptions import UnkSignalError
 from pytkwrap.gtk3._libs import Gdk, Gtk
-from pytkwrap.gtk3.buttons import GTK3ColorButton
+from pytkwrap.gtk3.buttons import GTK3FontButton
 from pytkwrap.gtk3.widget import GTK3WidgetProperties
 
 # pytkwrap Local Imports
@@ -20,34 +20,38 @@ from .conftest import BaseGTK3WidgetTests
 
 # @pytest.mark.skip(
 #    reason=(
-#        "Gtk.ColorButton routinely crashes at the C level in test environments. "
+#        "Gtk.FontButton routinely crashes at the C level in test environments. "
 #        "Requires manual testing in a running GTK3 application."
 #        "See examples/color_button.py for manual test program."
 #    )
 # )
 @pytest.mark.usefixtures("suppress_stderr")
-class TestGTK3ColorButton(BaseGTK3WidgetTests):
-    """Test class for the GTK3ColorButton."""
+class TestGTK3FontButton(BaseGTK3WidgetTests):
+    """Test class for the GTK3FontButton."""
 
-    widget_class = GTK3ColorButton
+    widget_class = GTK3FontButton
     expected_attributes = [
-        "get_alpha",
-        "get_color",
+        "get_font_name",
+        "get_show_size",
+        "get_show_style",
         "get_title",
-        "get_use_alpha",
-        "set_alpha",
-        "set_color",
+        "get_use_font",
+        "get_use_size",
+        "set_font_name",
+        "set_show_size",
+        "set_show_style",
         "set_title",
-        "set_use_alpha",
+        "set_use_font",
+        "set_use_size",
     ]
-    expected_default_edit_signal = "color-set"
+    expected_default_edit_signal = "font-set"
     expected_default_height = 30
     expected_default_value = None
     expected_default_width = 60
     expected_handler_id = {
         "add": -1,
         "check-resize": -1,
-        "color-set": -1,
+        "font-set": -1,
         "destroy": -1,
         "direction-changed": -1,
         "hide": -1,
@@ -66,11 +70,11 @@ class TestGTK3ColorButton(BaseGTK3WidgetTests):
         "unrealize": -1,
     }
     expected_properties = {
-        "alpha": 65535,
         "border_width": 0,
         "can_default": False,
         "can_focus": False,
         "focus_on_click": True,
+        "font_name": "Sans 12",
         "halign": Gtk.Align.FILL,
         "has_default": False,
         "has_focus": False,
@@ -88,14 +92,15 @@ class TestGTK3ColorButton(BaseGTK3WidgetTests):
         "opacity": 1.0,
         "parent": None,
         "receives_default": False,
-        "rgba": None,
         "scale_factor": 1,
         "sensitive": True,
-        "show_editor": False,
-        "title": "Pick a Color",
+        "show_size": True,
+        "show_style": True,
+        "title": "Pick a Font",
         "tooltip_markup": "Missing tooltip, please file an issue to have one added.",
         "tooltip_text": "Missing tooltip, please file an issue to have one added.",
-        "use_alpha": True,
+        "use_font": False,
+        "use_size": False,
         "valign": Gtk.Align.FILL,
         "vexpand": False,
         "vexpand_set": False,
@@ -106,7 +111,7 @@ class TestGTK3ColorButton(BaseGTK3WidgetTests):
 
     @pytest.mark.unit
     def test_init(self):
-        """Should initialize an instance of a GTK3ColorButton."""
+        """Should initialize an instance of a GTK3FontButton."""
         dut = self.make_dut()
 
         assert isinstance(dut, self.widget_class)
@@ -116,8 +121,8 @@ class TestGTK3ColorButton(BaseGTK3WidgetTests):
         assert dut._DEFAULT_TOOLTIP == self.expected_default_tooltip
         assert dut._DEFAULT_WIDTH == self.expected_default_width
         assert dut.dic_attributes == {
-            "default_value": None,
-            "edit_signal": "color-set",
+            "default_value": "Sans 12",
+            "edit_signal": "font-set",
             "index": -1,
             "x_pos": 0,
             "y_pos": 0,
@@ -127,19 +132,17 @@ class TestGTK3ColorButton(BaseGTK3WidgetTests):
 
     @pytest.mark.unit
     def test_set_properties_default(self):
-        """Should set the default properties of a GTK3ColorButton when passed an empty
+        """Should set the default properties of a GTK3FontButton when passed an empty
         GTK3WidgetProperties."""
         dut = self.make_dut()
         dut.do_set_properties(GTK3WidgetProperties())
 
-        assert isinstance(dut.get_property("rgba"), Gdk.RGBA)
-        assert dut.get_property("rgba").alpha == 1.0
-        assert dut.get_property("rgba").blue == 0.0
-        assert dut.get_property("rgba").green == 0.0
-        assert dut.get_property("rgba").red == 0.0
-        assert not dut.get_property("show-editor")
-        assert dut.get_property("title") == "Pick a Color"
-        assert dut.get_property("use-alpha")
+        assert dut.get_property("font_name") == "Sans 12"
+        assert dut.get_property("show_size")
+        assert dut.get_property("show_style")
+        assert dut.get_property("title") == "Pick a Font"
+        assert not dut.get_property("use_font")
+        assert not dut.get_property("use_size")
 
     @pytest.mark.unit
     def test_set_properties(self):
@@ -148,42 +151,37 @@ class TestGTK3ColorButton(BaseGTK3WidgetTests):
         dut = self.make_dut()
         dut.do_set_properties(
             GTK3WidgetProperties(
-                rgba=Gdk.RGBA(1.0, 1.0, 1.0, 1.0),
-                show_editor=True,
-                title="Choose a Color",
-                use_alpha=False,
+                font_name="Sans 10",
+                show_size=False,
+                show_style=False,
+                title="Choose a Font",
+                use_font=True,
+                use_size=True,
             )
         )
 
-        assert isinstance(dut.get_property("rgba"), Gdk.RGBA)
-        assert dut.get_property("rgba").alpha == 1.0
-        assert dut.get_property("rgba").blue == 1.0
-        assert dut.get_property("rgba").green == 1.0
-        assert dut.get_property("rgba").red == 1.0
-        assert dut.get_property("show-editor")
-        assert dut.get_property("title") == "Choose a Color"
-        assert not dut.get_property("use-alpha")
+        assert dut.do_get_property("font_name") == "Sans 10"
+        assert not dut.do_get_property("show_size")
+        assert not dut.do_get_property("show_style")
+        assert dut.do_get_property("title") == "Choose a Font"
+        assert dut.do_get_property("use_font")
+        assert dut.do_get_property("use_size")
 
     @pytest.mark.unit
     def test_do_update(self):
-        """Should update the GTK3ColorButton with the data package value."""
+        """Should update the GTK3FontButton with the data package value."""
         dut = self.make_dut()
         dut.dic_attributes["index"] = 1
         dut.do_set_callbacks(dut.dic_attributes["edit_signal"], dut.do_update)
         pub.subscribe(dut.do_update, "rootTopic")
 
-        pub.sendMessage("rootTopic", package={1: Gdk.RGBA(0.3, 0.1, 0.9, 0.75)})
+        pub.sendMessage("rootTopic", package={1: "Serif 15"})
 
-        assert isinstance(dut.get_property("rgba"), Gdk.RGBA)
-        assert isinstance(dut.get_rgba(), Gdk.RGBA)
-        assert dut.get_property("rgba").alpha == 0.75
-        assert dut.get_property("rgba").blue == 0.9
-        assert dut.get_property("rgba").green == 0.1
-        assert dut.get_property("rgba").red == 0.3
+        assert dut.do_get_property("font_name") == "Serif 15"
 
     @pytest.mark.unit
     def test_do_update_none_value(self):
-        """Should update the GTK3ColorButton properties with the default values when
+        """Should update the GTK3FontButton properties with the default values when
         passed a data package with a value of None."""
         dut = self.make_dut()
         dut.dic_attributes["index"] = 11
@@ -192,12 +190,7 @@ class TestGTK3ColorButton(BaseGTK3WidgetTests):
 
         pub.sendMessage("rootTopic", package={11: None})
 
-        assert isinstance(dut.get_property("rgba"), Gdk.RGBA)
-        assert isinstance(dut.get_rgba(), Gdk.RGBA)
-        assert dut.get_property("rgba").alpha == 1.0
-        assert dut.get_property("rgba").blue == 0.0
-        assert dut.get_property("rgba").green == 0.0
-        assert dut.get_property("rgba").red == 0.0
+        assert dut.do_get_property("font_name") == "Sans 12"
 
     @pytest.mark.unit
     def test_do_update_unknown_signal(self):
@@ -212,55 +205,47 @@ class TestGTK3ColorButton(BaseGTK3WidgetTests):
         with pytest.raises(UnkSignalError):
             pub.sendMessage("rootTopic", package={21: "Test Package"})
 
-        assert isinstance(dut.get_property("rgba"), Gdk.RGBA)
-        assert isinstance(dut.get_rgba(), Gdk.RGBA)
-        assert dut.get_property("rgba").alpha == 1.0
-        assert dut.get_property("rgba").blue == 0.0
-        assert dut.get_property("rgba").green == 0.0
-        assert dut.get_property("rgba").red == 0.0
+        assert dut.do_get_property("font_name") == "Sans 12"
 
     @pytest.mark.unit
     def test_do_update_wrong_field(self):
-        """Should do nothing when the data package key doesn't match the GTK3ColorButton
+        """Should do nothing when the data package key doesn't match the GTK3FontButton
         field name."""
         dut = self.make_dut()
         dut.dic_attributes["index"] = 22
         dut.do_set_callbacks(dut.dic_attributes["edit_signal"], dut.on_changed)
         pub.subscribe(dut.do_update, "rootTopic")
-        dut.set_rgba(Gdk.RGBA(0.5, 0.5, 0.5, 0.88))
+        dut.set_font("Helvetica 12")
 
         pub.sendMessage("rootTopic", package={12: False})
 
-        assert dut.get_rgba().alpha == 0.88
-        assert dut.get_rgba().blue == 0.5
-        assert dut.get_rgba().green == 0.5
-        assert dut.get_rgba().red == 0.5
+        assert dut.do_get_property("font_name") == "Sans 12"
 
     @pytest.mark.unit
     def test_on_changed(self):
-        """on_changed() is called when the GTK3ColorButton color is set."""
+        """on_changed() is called when the GTK3FontButton color is set."""
         dut = self.make_dut()
         dut.dic_attributes["index"] = 5
-        dut.dic_attributes["send_topic"] = "color_changed"
+        dut.dic_attributes["send_topic"] = "font_changed"
         dut.do_set_callbacks(dut.dic_attributes["edit_signal"], dut.on_changed)
 
         pub.subscribe(self.mock_handler, dut.dic_attributes["send_topic"])
 
-        dut.emit("color-set")
+        dut.emit("font-set")
 
     @pytest.mark.skip(
         reason="GTK3Widget.on_changed() does not raise an UnkSignalError for some "
         "reason."
     )
     def test_on_changed_unknown_signal(self):
-        """Should raise a UnkSignalError with an unknown edit signal name."""
+        """Should raise an UnkSignalError with an unknown edit signal name."""
         dut = self.make_dut()
         dut.dic_attributes["index"] = 15
-        dut.dic_attributes["send_topic"] = "color_changed"
+        dut.dic_attributes["send_topic"] = "font_changed"
         dut.do_set_callbacks(dut.dic_attributes["edit_signal"], dut.on_changed)
         pub.subscribe(self.mock_handler, dut.dic_attributes["send_topic"])
         pub.subscribe(self.on_changed_error_handler, "do_log_error")
         dut.dic_attributes["edit_signal"] = "unk_signal"
 
         with pytest.raises(UnkSignalError):
-            dut.emit("color-set")
+            dut.emit("font-set")
