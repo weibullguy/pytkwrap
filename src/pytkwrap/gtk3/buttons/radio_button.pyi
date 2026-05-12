1a8c5b9ace59f1b44a90af44725ebc5a6613e7c1
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-05-12 16:30:39 -0400

    feat: add GTK3RadioButton stub file

--- src/pytkwrap/gtk3/buttons/radio_button.pyi
+++ src/pytkwrap/gtk3/buttons/radio_button.pyi
@@ -7,15 +7,16 @@ from _typeshed import Incomplete
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.buttons.toggle_button import GTK3ToggleButton as GTK3ToggleButton
+from pytkwrap.gtk3.buttons.check_button import GTK3CheckButton as GTK3CheckButton
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3CheckButton(Gtk.CheckButton, GTK3ToggleButton):
+class GTK3RadioButton(Gtk.RadioButton, GTK3CheckButton):
     _DEFAULT_EDIT_SIGNAL: str
     _DEFAULT_HEIGHT: int
     _DEFAULT_WIDTH: int
-    _GTK3_CHECK_BUTTON_PROPERTIES: Incomplete
-    def __init__(self, label: str = "...") -> None: ...
+    _GTK3_RADIO_BUTTON_PROPERTIES: Incomplete
+    _GTK3_RADIO_BUTTON_SIGNALS: Incomplete
+    def __init__(self, label: str = "...", group=None) -> None: ...
     def do_get_property(
         self, property_name: str
     ) -> bool | date | float | int | object | str | None: ...
