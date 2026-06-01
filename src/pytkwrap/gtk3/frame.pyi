1b7f12ac3dd8872e68dae4a55e5e55e0c68e7652
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-06-01 12:37:03 -0400

    feat: add GTK3Frame stub file

--- src/pytkwrap/gtk3/frame.pyi
+++ src/pytkwrap/gtk3/frame.pyi
@@ -1,9 +1,21 @@
-from _typeshed import Incomplete
-from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.widget import GTK3BaseWidget as GTK3BaseWidget, GTK3WidgetProperties as GTK3WidgetProperties
+# Standard Library Imports
+from collections.abc import Mapping
+from datetime import date
 
-class GTK3Frame(Gtk.Frame, GTK3BaseWidget):
+# Third Party Imports
+from _typeshed import Incomplete
+
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.container import GTK3Container as GTK3Container
+from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
+
+class GTK3Frame(Gtk.Frame, GTK3Container):
     _GTK3_FRAME_PROPERTIES: Incomplete
-    _GTK3_FRAME_SIGNALS: Incomplete
     def __init__(self) -> None: ...
-    def do_set_properties(self, properties: GTK3WidgetProperties) -> None: ...
+    def do_get_property(
+        self, property_name: str
+    ) -> bool | date | float | int | object | str | None: ...
+    def do_set_properties(
+        self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
