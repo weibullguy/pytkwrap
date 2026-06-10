a50f8f7c906e51db71ad7864213ed613fe090a4c
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-06-09 22:37:54 -0400

    feat: add stub file for GTK3ImageMenuItem

---
+++ src/pytkwrap/gtk3/menu/imagemenuitem.pyi
@@ -0,0 +1,6 @@
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.menu.menuitem import GTK3MenuItem as GTK3MenuItem
+
+class GTK3ImageMenuItem(Gtk.ImageMenuItem, GTK3MenuItem):
+    def __init__(self) -> None: ...
