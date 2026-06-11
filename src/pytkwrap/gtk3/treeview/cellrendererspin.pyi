70f7138bc5c136c74c03e400c69c55b95fe1333a
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-06-10 21:48:57 -0400

    feat: add stub file for GTK3CellRendererSpin

---
+++ src/pytkwrap/gtk3/treeview/cellrendererspin.pyi
@@ -0,0 +1,19 @@
+# Standard Library Imports
+from collections.abc import Mapping
+
+# Third Party Imports
+from _typeshed import Incomplete
+
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
+from pytkwrap.gtk3.treeview.cellrenderertext import (
+    GTK3CellRendererText as GTK3CellRendererText,
+)
+
+class GTK3CellRendererSpin(Gtk.CellRendererSpin, GTK3CellRendererText):
+    _GTK3_CELLRENDERERSPIN_PROPERTIES: Incomplete
+    def __init__(self) -> None: ...
+    def do_set_properties(
+        self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
