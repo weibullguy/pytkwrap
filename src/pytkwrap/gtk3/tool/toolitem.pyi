1390f8325dfaccaf310c137a1ffe70dc9362e1b5
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-06-14 11:07:50 -0400

    feat: add stub file for GTK3ToolItem

---
+++ src/pytkwrap/gtk3/tool/toolitem.pyi
@@ -0,0 +1,18 @@
+# Standard Library Imports
+from collections.abc import Mapping
+
+# Third Party Imports
+from _typeshed import Incomplete
+
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.container.bin import GTK3Bin as GTK3Bin
+from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
+
+class GTK3ToolItem(Gtk.ToolItem, GTK3Bin):
+    _GTK3_TOOLITEM_PROPERTIES: Incomplete
+    _GTK3_TOOLITEM_SIGNALS: Incomplete
+    def __init__(self) -> None: ...
+    def do_set_properties(
+        self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
