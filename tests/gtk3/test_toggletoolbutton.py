daa32154a2646754db8dabc1ab93013df3602f2c
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-06-14 11:57:41 -0400

    test: add test file for GTK3ToggleToolButton

---
+++ tests/gtk3/test_toggletoolbutton.py
@@ -0,0 +1,92 @@
+"""Test module for the GTK3ToggleToolButton class.
+
+.. author:: Doyle Rowland
+.. copyright:: Since 2007, all rights reserved.
+"""
+
+# Third Party Imports
+import pytest
+
+# pytkwrap Package Imports
+# noinspection PyProtectedMember
+from pytkwrap.gtk3._libs import Gtk
+from pytkwrap.gtk3.mixins import GTK3WidgetProperties
+from pytkwrap.gtk3.tool import GTK3ToggleToolButton
+
+# pytkwrap Local Imports
+from .conftest import BaseGTK3GObjectTests
+from .test_constants import (
+    EXPECTED_BIN_METHODS,
+    EXPECTED_CONTAINER_HANDLER_IDS,
+    EXPECTED_CONTAINER_METHODS,
+    EXPECTED_CONTAINER_PROPERTIES,
+    EXPECTED_GOBJECT_ATTRIBUTES,
+    EXPECTED_GOBJECT_HANDLER_IDS,
+    EXPECTED_GOBJECT_METHODS,
+    EXPECTED_TOGGLETOOLBUTTON_HANDLER_IDS,
+    EXPECTED_TOGGLETOOLBUTTON_METHODS,
+    EXPECTED_TOGGLETOOLBUTTON_PROPERTIES,
+    EXPECTED_TOOLBUTTON_HANDLER_IDS,
+    EXPECTED_TOOLBUTTON_METHODS,
+    EXPECTED_TOOLBUTTON_PROPERTIES,
+    EXPECTED_TOOLITEM_HANDLER_IDS,
+    EXPECTED_TOOLITEM_METHODS,
+    EXPECTED_TOOLITEM_PROPERTIES,
+    EXPECTED_WIDGET_ATTRIBUTES,
+    EXPECTED_WIDGET_HANDLER_IDS,
+    EXPECTED_WIDGET_METHODS,
+    EXPECTED_WIDGET_PROPERTIES,
+)
+
+
+class TestGTK3ToggleToolButton(BaseGTK3GObjectTests):
+    """Test class for the GTK3ToggleToolButton class."""
+
+    widget_class = GTK3ToggleToolButton
+    expected_attributes = EXPECTED_GOBJECT_ATTRIBUTES | EXPECTED_WIDGET_ATTRIBUTES
+    expected_handler_id = (
+        EXPECTED_GOBJECT_HANDLER_IDS
+        | EXPECTED_WIDGET_HANDLER_IDS
+        | EXPECTED_CONTAINER_HANDLER_IDS
+        | EXPECTED_TOOLITEM_HANDLER_IDS
+        | EXPECTED_TOOLBUTTON_HANDLER_IDS
+        | EXPECTED_TOGGLETOOLBUTTON_HANDLER_IDS
+    )
+    expected_methods = (
+        EXPECTED_GOBJECT_METHODS
+        + EXPECTED_WIDGET_METHODS
+        + EXPECTED_CONTAINER_METHODS
+        + EXPECTED_BIN_METHODS
+        + EXPECTED_TOOLITEM_METHODS
+        + EXPECTED_TOOLBUTTON_METHODS
+        + EXPECTED_TOGGLETOOLBUTTON_METHODS
+    )
+    expected_properties = (
+        EXPECTED_WIDGET_PROPERTIES
+        | EXPECTED_CONTAINER_PROPERTIES
+        | EXPECTED_TOOLITEM_PROPERTIES
+        | EXPECTED_TOOLBUTTON_PROPERTIES
+        | EXPECTED_TOGGLETOOLBUTTON_PROPERTIES
+    )
+
+    @pytest.mark.unit
+    def test_do_set_properties_default(self):
+        """Should set properties to default values when passed an empty
+        GTK3WidgetProperties."""
+        dut = self.make_dut()
+        dut.do_set_properties(GTK3WidgetProperties())
+
+        assert dut.dic_properties == self.expected_properties
+        assert not dut.do_get_property("active")
+
+    @pytest.mark.unit
+    def test_do_set_properties(self):
+        """Should set properties to the values passed in the GTK3WidgetProperties."""
+        dut = self.make_dut()
+        dut.do_set_properties(
+            GTK3WidgetProperties(
+                active=True,
+            )
+        )
+
+        assert dut.get_property("active")
