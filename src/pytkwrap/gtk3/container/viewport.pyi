521b89a099771999dc528ddf9d296d2491c60984
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-06-14 12:34:11 -0400

    feat: add stub file for GTK3Viewport

---
+++ src/pytkwrap/gtk3/container/viewport.pyi
@@ -0,0 +1,17 @@
+# Standard Library Imports
+from collections.abc import Mapping
+
+# Third Party Imports
+from _typeshed import Incomplete
+
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.container.bin import GTK3Bin as GTK3Bin
+from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
+
+class GTK3Viewport(Gtk.Viewport, GTK3Bin):
+    _GTK3_VIEWPORT_PROPERTIES: Incomplete
+    def __init__(self) -> None: ...
+    def do_set_properties(
+        self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
