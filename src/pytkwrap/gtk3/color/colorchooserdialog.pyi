aba60b6cbf430a004642b8f58647cc49e81581b9
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-06-17 15:35:35 -0400

    feat: add stub file for GTK3ColorChooserDialog

---
+++ src/pytkwrap/gtk3/color/colorchooserdialog.pyi
@@ -0,0 +1,17 @@
+# Standard Library Imports
+from collections.abc import Mapping
+
+# Third Party Imports
+from _typeshed import Incomplete
+
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.dialog.dialog import GTK3Dialog as GTK3Dialog
+from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
+
+class GTK3ColorChooserDialog(Gtk.ColorChooserDialog, GTK3Dialog):
+    _GTK3_COLORCHOOSERDIALOG_PROPERTIES: Incomplete
+    def __init__(self) -> None: ...
+    def do_set_properties(
+        self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
