c4eb32ff8a5cc040370959f0a17513105380ef0e
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-04 11:11:46 -0400

    test: add test file for GTK3ToolPalette

---
+++ tests/gtk3/tool/test_toolpalette.py
@@ -0,0 +1,89 @@
+"""Test module for the GTK3ToolPalette class.
+
+.. author:: Doyle Rowland
+.. copyright:: Since 2007, all rights reserved.
+"""
+
+# Third Party Imports
+import pytest
+
+# pytkwrap Package Imports
+# noinspection PyProtectedMember
+from pytkwrap.gtk3._libs import Gtk
+from pytkwrap.gtk3.mixins import GTK3WidgetAttributes, GTK3WidgetProperties
+from pytkwrap.gtk3.tool import GTK3ToolPalette
+from tests.gtk3.conftest import BaseGTK3GObjectTests
+from tests.gtk3.constants import (
+    EXPECTED_GOBJECT_ATTRIBUTES,
+    EXPECTED_GOBJECT_HANDLER_IDS,
+    EXPECTED_GOBJECT_METHODS,
+    EXPECTED_WIDGET_ATTRIBUTES,
+    EXPECTED_WIDGET_HANDLER_IDS,
+    EXPECTED_WIDGET_METHODS,
+    EXPECTED_WIDGET_PROPERTIES,
+)
+from tests.gtk3.container.constants import (
+    EXPECTED_CONTAINER_HANDLER_IDS,
+    EXPECTED_CONTAINER_METHODS,
+    EXPECTED_CONTAINER_PROPERTIES,
+)
+from tests.gtk3.tool.constants import (
+    EXPECTED_TOOLPALETTE_METHODS,
+    EXPECTED_TOOLPALETTE_PROPERTIES,
+)
+
+
+@pytest.mark.usefixtures("suppress_stderr")
+class TestGTK3ToolPalette(BaseGTK3GObjectTests):
+    """Test class for the GTK3ToolPalette class."""
+
+    widget_class = GTK3ToolPalette
+    expected_attributes = EXPECTED_GOBJECT_ATTRIBUTES | EXPECTED_WIDGET_ATTRIBUTES
+    expected_default_height = -1
+    expected_default_width = -1
+    expected_handler_id = (
+        EXPECTED_GOBJECT_HANDLER_IDS
+        | EXPECTED_WIDGET_HANDLER_IDS
+        | EXPECTED_CONTAINER_HANDLER_IDS
+    )
+    expected_methods = (
+        EXPECTED_GOBJECT_METHODS
+        + EXPECTED_WIDGET_METHODS
+        + EXPECTED_CONTAINER_METHODS
+        + EXPECTED_TOOLPALETTE_METHODS
+    )
+    expected_properties = (
+        EXPECTED_WIDGET_PROPERTIES
+        | EXPECTED_CONTAINER_PROPERTIES
+        | EXPECTED_TOOLPALETTE_PROPERTIES
+    )
+
+    @pytest.mark.unit
+    def test_do_set_properties_default(self):
+        """Should set properties to default values when passed an empty
+        GTK3WidgetProperties."""
+        dut = self.make_dut()
+        dut.do_set_properties(GTK3WidgetProperties())
+
+        assert dut.dic_properties == self.expected_properties
+        assert dut.do_get_property("icon_size") == Gtk.IconSize.SMALL_TOOLBAR
+        assert not dut.do_get_property("icon_size_set")
+        assert dut.do_get_property("toolbar_style") == Gtk.ToolbarStyle.ICONS
+
+    @pytest.mark.unit
+    def test_do_set_properties(self):
+        """Should set properties to the values passed in the GTK3WidgetProperties."""
+        dut = self.make_dut()
+        dut.do_set_properties(
+            GTK3WidgetProperties(
+                icon_size=Gtk.IconSize.BUTTON,
+                icon_size_set=True,
+                toolbar_style=Gtk.ToolbarStyle.BOTH,
+            )
+        )
+
+        assert dut.get_property("icon_size") == Gtk.IconSize.BUTTON
+        assert dut.get_icon_size() == Gtk.IconSize.BUTTON
+        assert dut.get_property("icon_size_set")
+        assert dut.get_property("toolbar_style") == Gtk.ToolbarStyle.BOTH
+        assert dut.get_style() == Gtk.ToolbarStyle.BOTH
