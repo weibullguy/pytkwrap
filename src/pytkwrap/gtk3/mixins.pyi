d4d75851191ae18fc6b22042b8eb2229122abb4a
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-21 10:02:59 -0400

    feat: add stub file for GTK3PrintOperation

--- src/pytkwrap/gtk3/mixins.pyi
+++ src/pytkwrap/gtk3/mixins.pyi
@@ -43,6 +43,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     align_set: bool
     align_widget: Gtk.Container | None
     alignment: float | Pango.Alignment | None
+    allow_async: bool
     alpha: int
     always_show_image: bool
     anchor_hints: Gdk.AnchorHints
@@ -93,15 +94,18 @@ class GTK3WidgetProperties(TypedDict, total=False):
     copyright: str | None
     create_folders: bool
     current_alpha: int
+    current_page: int
     current_rgba: Gdk.RGBA | None
     cursor_position: int
     cursor_visible: bool
+    custom_tab_label: str | None
     custom_title: str | None
     day: int
     decorated: bool
     decoration_layout: Gtk.DecorationLayout | None
     decoration_layout_set: bool
     default_height: int
+    default_page_setup: Gtk.PageSetup | None
     default_width: int
     deletable: bool
     destroy_with_parent: bool
@@ -123,6 +127,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     editing_canceled: bool
     ellipsize: Pango.EllipsizeMode | None
     ellipsize_set: bool
+    embed_page_setup: bool
     enable_emoji_completion: bool
     enable_grid_lines: Gtk.TreeViewGridLines | None
     enable_popup: bool
@@ -133,6 +138,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     expand: bool
     expanded: bool
     expander_column: Gtk.TreeViewColumn | None
+    export_filename: str | None
     extra_widget: Gtk.Widget | None
     family: str | None
     family_set: bool
@@ -169,6 +175,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     has_origin: bool
     has_palette: bool
     has_resize_grip: bool
+    has_selection: bool
     has_stencil_buffer: bool
     has_subtitle: bool
     has_tooltip: bool
@@ -220,6 +227,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     item_orientation: Gtk.Orientation | None
     item_padding: int
     item_width: int
+    job_name: str
     justification: Gtk.Justification | None
     justify: Gtk.Justification | None
     kinetic_scrolling: bool
@@ -275,6 +283,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     monitor: int
     monospace: bool
     month: int
+    n_pages: int
     name: str
     no_month_change: bool
     no_show_all: bool
@@ -321,6 +330,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     primary_icon_storage_type: Gtk.ImageType | None
     primary_icon_tooltip_markup: str | None
     primary_icon_tooltip_text: str | None
+    print_settings: Gtk.PrintSettings | None
     program_name: str | None
     progress_fraction: float
     progress_pulse_step: float
@@ -397,6 +407,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     show_menubar: bool
     show_numbers: bool
     show_other_locations: bool
+    show_progress: bool
     show_recent: bool
     show_size: bool
     show_starred_location: bool
@@ -434,6 +445,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     submenu: Gtk.Menu | None
     subtitle: str | None
     subtitle_set: bool
+    support_selection: bool
     surface: cairo.Surface | None
     tabs: Pango.TabArray | None
     tab_pos: Gtk.PositionType
@@ -451,6 +463,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     tooltip_markup: str
     tooltip_text: str
     top_margin: int
+    track_print_status: bool
     track_visited_links: bool
     transient_for: Gtk.Window | None
     transition_duration: int
@@ -461,6 +474,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     type_hint: Gdk.WindowTypeHint
     underline: Pango.Underline | None
     underline_set: bool
+    unit: Gtk.Unit
     update_policy: Gtk.SpinButtonUpdatePolicy | None
     upper: float
     urgency_hint: bool
@@ -468,6 +482,7 @@ class GTK3WidgetProperties(TypedDict, total=False):
     use_es: bool
     use_fallback: bool
     use_font: bool
+    use_full_page: bool
     use_header_bar: bool | int
     use_markup: bool
     use_popover: bool
---
+++ src/pytkwrap/gtk3/print/printoperation.pyi
@@ -0,0 +1,19 @@
+# Standard Library Imports
+from collections.abc import Mapping
+
+# Third Party Imports
+from _typeshed import Incomplete
+
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
+from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
+
+class GTK3PrintOperation(Gtk.PrintOperation, GTK3GObjectMixin):
+    _GTK3_PRINTOPERATION_PROPERTIES: Incomplete
+    _GTK3_PRINTOPERATION_SIGNALS: Incomplete
+    dic_properties: Incomplete
+    def __init__(self) -> None: ...
+    def do_set_properties(
+        self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
