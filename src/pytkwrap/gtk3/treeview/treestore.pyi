aa0f72b41abd66b7a3d633003f92a5b73196f071
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-25 13:23:39 -0400

    feat: add stub file for GTK3TreeStore

---
+++ src/pytkwrap/gtk3/treeview/treestore.pyi
@@ -0,0 +1,21 @@
+# Standard Library Imports
+from collections.abc import Mapping
+from datetime import date
+
+# Third Party Imports
+from _typeshed import Incomplete
+
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
+from pytkwrap.gtk3.mixins import GTK3WidgetAttributes as GTK3WidgetAttributes
+
+class GTK3TreeStore(Gtk.TreeStore, GTK3GObjectMixin):
+    _GTK3_TREESTORE_ATTRIBUTES: Incomplete
+    def __init__(
+        self, column_types: list | None, n_columns: int, n_rows: int
+    ) -> None: ...
+    def do_get_attribute(
+        self, attribute: str
+    ) -> bool | date | float | int | object | str | None: ...
+    def do_set_attributes(self, attributes: Mapping[str, object]) -> None: ...
