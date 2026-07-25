9078675e1254efbd1768fbbe31278bcf28b02fa5
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-25 12:57:40 -0400

    feat: add stub file for GTK3TreeModelSort

---
+++ src/pytkwrap/gtk3/treeview/treemodelsort.pyi
@@ -0,0 +1,11 @@
+# Third Party Imports
+from _typeshed import Incomplete
+
+# pytkwrap Package Imports
+from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
+from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
+
+class GTK3TreeModelSort(Gtk.TreeModelSort, GTK3GObjectMixin):
+    _GTK3_TREEMODELSORT_PROPERTIES: Incomplete
+    def __init__(self, model: Gtk.TreeModel | None) -> None: ...
