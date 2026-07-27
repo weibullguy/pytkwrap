2a2f912028a0f483a74cccc0843c8bda935c5adf
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-26 21:01:17 -0400

    feat: update stub file for GTK3SearchBar

--- src/pytkwrap/gtk3/bar/searchbar.pyi
+++ src/pytkwrap/gtk3/bar/searchbar.pyi
@@ -1,3 +1,6 @@
+# Standard Library Imports
+from collections.abc import Mapping
+
 # Third Party Imports
 from _typeshed import Incomplete
 
@@ -9,3 +12,6 @@ from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 class GTK3SearchBar(Gtk.SearchBar, GTK3Bin):
     _GTK3_SEARCHBAR_PROPERTIES: Incomplete
     def __init__(self) -> None: ...
+    def do_set_properties(
+        self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
