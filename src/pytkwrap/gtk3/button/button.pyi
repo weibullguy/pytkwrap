3fc140750261d8a83b3824a39499936dcae56e8b
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-28 23:30:58 -0400

    refactor: update stub files for GTK3Button and GTK3ButtonBox

--- src/pytkwrap/gtk3/button/button.pyi
+++ src/pytkwrap/gtk3/button/button.pyi
@@ -1,5 +1,4 @@
 # Standard Library Imports
-from collections.abc import Callable as Callable
 from collections.abc import Mapping
 
 # Third Party Imports
@@ -8,25 +7,15 @@ from _typeshed import Incomplete
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import GdkPixbuf as GdkPixbuf
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.bin import GTK3Bin as GTK3Bin
+from pytkwrap.gtk3.container.bin import GTK3Bin as GTK3Bin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
-
-def do_make_buttonbox(
-    icons: list[str],
-    tooltips: list[str],
-    callbacks: list[Callable],
-    height: int = -1,
-    layout: Gtk.ButtonBoxStyle = ...,
-    orientation: str = "vertical",
-    width: int = -1,
-) -> Gtk.ButtonBox: ...
 
 class GTK3Button(Gtk.Button, GTK3Bin):
     _DEFAULT_HEIGHT: int
     _DEFAULT_WIDTH: int
     _GTK3_BUTTON_PROPERTIES: Incomplete
     _GTK3_BUTTON_SIGNALS: Incomplete
-    def __init__(self, label: str = "...") -> None: ...
+    def __init__(self, label: str | None = "...") -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
--- src/pytkwrap/gtk3/container/buttonbox.pyi
+++ src/pytkwrap/gtk3/container/buttonbox.pyi
@@ -1,11 +1,14 @@
 # Standard Library Imports
+from collections.abc import Callable as Callable
 from collections.abc import Mapping
 
 # Third Party Imports
 from _typeshed import Incomplete
 
 # pytkwrap Package Imports
+from pytkwrap.gtk3._libs import GdkPixbuf as GdkPixbuf
 from pytkwrap.gtk3._libs import Gtk as Gtk
+from pytkwrap.gtk3.button.button import GTK3Button as GTK3Button
 from pytkwrap.gtk3.container.box import GTK3Box as GTK3Box
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
@@ -15,3 +18,13 @@ class GTK3ButtonBox(Gtk.ButtonBox, GTK3Box):
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+
+def do_make_buttonbox(
+    icons: list[str],
+    tooltips: list[str],
+    callbacks: list[Callable],
+    height: int = -1,
+    layout: Gtk.ButtonBoxStyle = ...,
+    orientation: str = "horizontal",
+    width: int = -1,
+) -> GTK3ButtonBox: ...
