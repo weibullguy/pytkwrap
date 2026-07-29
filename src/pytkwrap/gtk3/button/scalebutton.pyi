f29392d6ca4bd697036ecd45ebafa6865fe4bad5
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-29 00:13:47 -0400

    feat: update stub file for GTK3ScaleButton

--- src/pytkwrap/gtk3/button/scalebutton.pyi
+++ src/pytkwrap/gtk3/button/scalebutton.pyi
@@ -8,7 +8,8 @@ from _typeshed import Incomplete
 # pytkwrap Package Imports
 from pytkwrap.common.mixins import PyTkWrapAttributes as PyTkWrapAttributes
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.buttons import GTK3Button as GTK3Button
+from pytkwrap.gtk3.adjustment import GTK3Adjustment as GTK3Adjustment
+from pytkwrap.gtk3.button.button import GTK3Button as GTK3Button
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
 class GTK3ScaleButton(Gtk.ScaleButton, GTK3Button):
@@ -17,7 +18,14 @@ class GTK3ScaleButton(Gtk.ScaleButton, GTK3Button):
     _GTK3_SCALE_BUTTON_ATTRIBUTES: PyTkWrapAttributes
     _GTK3_SCALE_BUTTON_PROPERTIES: Incomplete
     _GTK3_SCALE_BUTTON_SIGNALS: Incomplete
-    def __init__(self) -> None: ...
+    def __init__(
+        self,
+        size: int,
+        min_value: float = 0.0,
+        max_value: float = 100.0,
+        step: float = 2,
+        icons: list | None = None,
+    ) -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
