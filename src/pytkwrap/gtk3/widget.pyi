67555b7fb33a1370c98d0161f7fba50781efe0c8
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-31 19:58:46 -0400

    refactor: update stub file for GTK3Widget

--- src/pytkwrap/gtk3/widget.pyi
+++ src/pytkwrap/gtk3/widget.pyi
@@ -11,7 +11,7 @@ from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetAttributes as GTK3WidgetAttributes
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3Widget(Gtk.Widget, GTK3GObjectMixin):
+class GTK3WidgetMixin(GTK3GObjectMixin):
     _DEFAULT_HEIGHT: int
     _DEFAULT_WIDTH: int
     _GTK3_WIDGET_ATTRIBUTES: Incomplete
@@ -22,3 +22,6 @@ class GTK3Widget(Gtk.Widget, GTK3GObjectMixin):
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+
+class GTK3Widget(Gtk.Widget, GTK3WidgetMixin):
+    def __init__(self) -> None: ...
