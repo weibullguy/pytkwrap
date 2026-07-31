2b72808242566f0c25478319d3613aad673bf843
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-30 22:36:39 -0400

    feat: update stub file for GTK3VolumeButton

--- src/pytkwrap/gtk3/button/volumebutton.pyi
+++ src/pytkwrap/gtk3/button/volumebutton.pyi
@@ -1,9 +1,12 @@
+# Standard Library Imports
+from collections.abc import Mapping
+
 # Third Party Imports
 from _typeshed import Incomplete
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.buttons.scale_button import GTK3ScaleButton as GTK3ScaleButton
+from pytkwrap.gtk3.button.scalebutton import GTK3ScaleButton as GTK3ScaleButton
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
 class GTK3VolumeButton(Gtk.VolumeButton, GTK3ScaleButton):
@@ -11,3 +14,6 @@ class GTK3VolumeButton(Gtk.VolumeButton, GTK3ScaleButton):
     _DEFAULT_WIDTH: int
     _GTK3_VOLUME_BUTTON_PROPERTIES: Incomplete
     def __init__(self) -> None: ...
+    def do_set_properties(
+        self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
