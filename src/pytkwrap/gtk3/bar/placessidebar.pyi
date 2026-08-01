3224c57a46c3e7d64b99ed8b307c413feb5facca
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-01 13:51:09 -0400

    refactor: update stub file for GTK3PlacesSidebar

--- src/pytkwrap/gtk3/bar/placessidebar.pyi
+++ src/pytkwrap/gtk3/bar/placessidebar.pyi
@@ -7,12 +7,17 @@ from _typeshed import Incomplete
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
-from pytkwrap.gtk3.window.scrolledwindow import GTK3ScrolledWindow as GTK3ScrolledWindow
+from pytkwrap.gtk3.window.scrolledwindow import (
+    GTK3ScrolledWindowMixin as GTK3ScrolledWindowMixin,
+)
 
-class GTK3PlacesSidebar(Gtk.PlacesSidebar, GTK3ScrolledWindow):
+class GTK3PlacesSidebarMixin(GTK3ScrolledWindowMixin):
     _GTK3_PLACESSIDEBAR_PROPERTIES: Incomplete
     _GTK3_PLACESSIDEBAR_SIGNALS: Incomplete
     def __init__(self) -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+
+class GTK3PlacesSidebar(Gtk.PlacesSidebar, GTK3PlacesSidebarMixin):
+    def __init__(self) -> None: ...
