5d8545a0f8136afe74ed5f53819871c2641cc094
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-01 13:47:34 -0400

    refactor: update stub file for GTK3HeaderBar

--- src/pytkwrap/gtk3/bar/headerbar.pyi
+++ src/pytkwrap/gtk3/bar/headerbar.pyi
@@ -6,12 +6,15 @@ from _typeshed import Incomplete
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.container import GTK3Container as GTK3Container
+from pytkwrap.gtk3.container import GTK3ContainerMixin as GTK3ContainerMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3HeaderBar(Gtk.HeaderBar, GTK3Container):
+class GTK3HeaderBarMixin(GTK3ContainerMixin):
     _GTK3_HEADERBAR_PROPERTIES: Incomplete
     def __init__(self) -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+
+class GTK3HeaderBar(Gtk.HeaderBar, GTK3HeaderBarMixin):
+    def __init__(self) -> None: ...
