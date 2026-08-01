8bfa9325e422dfb3addec853a2f17dc561b40d80
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-31 20:00:47 -0400

    refactor: create pure python mixin for GTK3Container

--- src/pytkwrap/gtk3/container/container.py
+++ src/pytkwrap/gtk3/container/container.py
@@ -10,11 +10,11 @@ from collections.abc import Mapping
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties
-from pytkwrap.gtk3.widget import GTK3Widget
+from pytkwrap.gtk3.widget import GTK3WidgetMixin
 
 
-class GTK3Container(Gtk.Container, GTK3Widget):
-    """Wrapper for version 3.0 Gtk.Container."""
+class GTK3ContainerMixin(GTK3WidgetMixin):
+    """Mixin class for GTK3Container."""
 
     _GTK3_CONTAINER_PROPERTIES = GTK3WidgetProperties(
         border_width=0,
@@ -28,9 +28,9 @@ class GTK3Container(Gtk.Container, GTK3Widget):
 
     def __init__(self) -> None:
         """Initialize an instance of the GTK3Container."""
-        Gtk.Container.__init__(self)
-        GTK3Widget.__init__(self)
+        GTK3WidgetMixin.__init__(self)
 
+        # Initialize public instance attributes.
         self.dic_handler_id.update(
             {_signal: -1 for _signal in self._GTK3_CONTAINER_SIGNALS}
         )
@@ -52,3 +52,12 @@ class GTK3Container(Gtk.Container, GTK3Widget):
         super().do_set_properties(properties)
 
         self.set_border_width(self.dic_properties["border_width"])
+
+
+class GTK3Container(Gtk.Container, GTK3ContainerMixin):
+    """Wrapper for version 3.0 Gtk.Container."""
+
+    def __init__(self) -> None:
+        """Initialize an instance of the GTK3Container."""
+        Gtk.Container.__init__(self)
+        GTK3ContainerMixin.__init__(self)
