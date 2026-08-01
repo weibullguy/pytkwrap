d53299653230296c1d527f0c686a1675b171999b
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-31 20:06:32 -0400

    refactor: update stub file for GTK3Button

--- src/pytkwrap/gtk3/button/button.pyi
+++ src/pytkwrap/gtk3/button/button.pyi
@@ -7,15 +7,18 @@ from _typeshed import Incomplete
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import GdkPixbuf as GdkPixbuf
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.container.bin import GTK3Bin as GTK3Bin
+from pytkwrap.gtk3.container.bin import GTK3BinMixin as GTK3BinMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3Button(Gtk.Button, GTK3Bin):
+class GTK3ButtonMixin(GTK3BinMixin):
     _DEFAULT_HEIGHT: int
     _DEFAULT_WIDTH: int
     _GTK3_BUTTON_PROPERTIES: Incomplete
     _GTK3_BUTTON_SIGNALS: Incomplete
-    def __init__(self, label: str | None = "...") -> None: ...
+    def __init__(self) -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+
+class GTK3Button(Gtk.Button, GTK3ButtonMixin):
+    def __init__(self, label: str | None = "...") -> None: ...
