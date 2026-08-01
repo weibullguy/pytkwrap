418eca5fc9014f7167e47c655f6b1e68a3e8f6c3
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-31 20:05:19 -0400

    refactor: update stub file for GTK3Bin

--- src/pytkwrap/gtk3/container/bin.pyi
+++ src/pytkwrap/gtk3/container/bin.pyi
@@ -1,6 +1,9 @@
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.container import GTK3Container as GTK3Container
+from pytkwrap.gtk3.container.container import GTK3ContainerMixin as GTK3ContainerMixin
 
-class GTK3Bin(Gtk.Bin, GTK3Container):
+class GTK3BinMixin(GTK3ContainerMixin):
+    def __init__(self) -> None: ...
+
+class GTK3Bin(Gtk.Bin, GTK3BinMixin):
     def __init__(self) -> None: ...
