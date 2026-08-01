7bd67546350c7bbe40d699725ff03f508f241f73
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-07-31 20:01:26 -0400

    refactor: update stub file for GTK3Container

--- src/pytkwrap/gtk3/container/container.pyi
+++ src/pytkwrap/gtk3/container/container.pyi
@@ -7,12 +7,15 @@ from _typeshed import Incomplete
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
-from pytkwrap.gtk3.widget import GTK3Widget as GTK3Widget
+from pytkwrap.gtk3.widget import GTK3WidgetMixin as GTK3WidgetMixin
 
-class GTK3Container(Gtk.Container, GTK3Widget):
+class GTK3ContainerMixin(GTK3WidgetMixin):
     _GTK3_CONTAINER_PROPERTIES: Incomplete
     _GTK3_CONTAINER_SIGNALS: Incomplete
     def __init__(self) -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+
+class GTK3Container(Gtk.Container, GTK3ContainerMixin):
+    def __init__(self) -> None: ...
