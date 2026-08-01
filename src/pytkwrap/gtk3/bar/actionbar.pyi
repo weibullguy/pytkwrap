55e165050925a967b9d17137241459300386e3cd
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-01 13:46:19 -0400

    refactor: update stub file for GTK3ActionBar

--- src/pytkwrap/gtk3/bar/actionbar.pyi
+++ src/pytkwrap/gtk3/bar/actionbar.pyi
@@ -1,6 +1,9 @@
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.widget import GTK3Widget as GTK3Widget
+from pytkwrap.gtk3.widget import GTK3WidgetMixin as GTK3WidgetMixin
 
-class GTK3ActionBar(Gtk.ActionBar, GTK3Widget):
+class GTK3ActionBarMixin(GTK3WidgetMixin):
+    def __init__(self) -> None: ...
+
+class GTK3ActionBar(Gtk.ActionBar, GTK3ActionBarMixin):
     def __init__(self) -> None: ...
