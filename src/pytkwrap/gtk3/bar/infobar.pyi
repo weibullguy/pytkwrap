a92f7fcd17ead1151e3f1db3afa761ec1d69ef8d
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-01 13:48:37 -0400

    refactor: update stub file for GTK3InfoBar

--- src/pytkwrap/gtk3/bar/infobar.pyi
+++ src/pytkwrap/gtk3/bar/infobar.pyi
@@ -6,13 +6,16 @@ from _typeshed import Incomplete
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.container.box import GTK3Box as GTK3Box
+from pytkwrap.gtk3.container.box import GTK3BoxMixin as GTK3BoxMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3InfoBar(Gtk.InfoBar, GTK3Box):
+class GTK3InfoBarMixin(GTK3BoxMixin):
     _GTK3_INFOBAR_PROPERTIES: Incomplete
     _GTK3_INFOBAR_SIGNALS: Incomplete
     def __init__(self) -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+
+class GTK3InfoBar(Gtk.InfoBar, GTK3InfoBarMixin):
+    def __init__(self) -> None: ...
