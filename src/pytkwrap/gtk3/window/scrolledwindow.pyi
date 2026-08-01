79618ec34cb03e59be0d7645cbfa16732a8867c7
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-01 13:44:57 -0400

    refactor: update stub file for GTK3ScrolledWindow

--- src/pytkwrap/gtk3/window/scrolledwindow.pyi
+++ src/pytkwrap/gtk3/window/scrolledwindow.pyi
@@ -7,17 +7,20 @@ from _typeshed import Incomplete
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.adjustment import GTK3Adjustment as GTK3Adjustment
-from pytkwrap.gtk3.container.bin import GTK3Bin as GTK3Bin
+from pytkwrap.gtk3.container.bin import GTK3BinMixin as GTK3BinMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3ScrolledWindow(Gtk.ScrolledWindow, GTK3Bin):
+class GTK3ScrolledWindowMixin(GTK3BinMixin):
     _GTK3_SCROLLEDWINDOW_PROPERTIES: Incomplete
     _GTK3_SCROLLEDWINDOW_SIGNALS: Incomplete
+    def __init__(self) -> None: ...
+    def do_set_properties(
+        self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
+
+class GTK3ScrolledWindow(Gtk.ScrolledWindow, GTK3ScrolledWindowMixin):
     def __init__(
         self,
         hadjustment: GTK3Adjustment | None = None,
         vadjustment: GTK3Adjustment | None = None,
-    ) -> None: ...
-    def do_set_properties(
-        self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
