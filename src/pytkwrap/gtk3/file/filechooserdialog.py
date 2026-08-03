e38a54cc7516c1f3e8d67fbfbf052c40fd3570a4
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-02 22:47:54 -0400

    refactor: create pure python mixin for GTK3FileChooserDialog

--- src/pytkwrap/gtk3/file/filechooserdialog.py
+++ src/pytkwrap/gtk3/file/filechooserdialog.py
@@ -6,13 +6,17 @@
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
-from pytkwrap.gtk3.dialog.dialog import GTK3Dialog
+from pytkwrap.gtk3.dialog.dialog import GTK3DialogMixin
 
 
-class GTK3FileChooserDialog(Gtk.FileChooserDialog, GTK3Dialog):
+class GTK3FileChooserDialogMixin(GTK3DialogMixin):
+    """Mixin class for GTK3FileChooserDialog."""
+
+
+class GTK3FileChooserDialog(Gtk.FileChooserDialog, GTK3FileChooserDialogMixin):
     """Wrapper for version 3.0 Gtk.FileChooserDialog."""
 
     def __init__(self) -> None:
         """Initialize an instance of the GTK3FileChooserDialog."""
         Gtk.FileChooserDialog.__init__(self)
-        GTK3Dialog.__init__(self)
+        GTK3FileChooserDialogMixin.__init__(self)
--- src/pytkwrap/gtk3/file/filechooserdialog.pyi
+++ src/pytkwrap/gtk3/file/filechooserdialog.pyi
@@ -1,6 +1,8 @@
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.dialog.dialog import GTK3Dialog as GTK3Dialog
+from pytkwrap.gtk3.dialog.dialog import GTK3DialogMixin as GTK3DialogMixin
 
-class GTK3FileChooserDialog(Gtk.FileChooserDialog, GTK3Dialog):
+class GTK3FileChooserDialogMixin(GTK3DialogMixin): ...
+
+class GTK3FileChooserDialog(Gtk.FileChooserDialog, GTK3FileChooserDialogMixin):
     def __init__(self) -> None: ...
