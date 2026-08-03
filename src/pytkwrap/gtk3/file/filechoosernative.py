6237c4070e1c57cdcef54e87105860d8f66467e3
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-02 23:03:21 -0400

    refactor: create pure python mixin for GTK3FileChooserNative

--- src/pytkwrap/gtk3/file/filechoosernative.py
+++ src/pytkwrap/gtk3/file/filechoosernative.py
@@ -9,22 +9,21 @@ from collections.abc import Mapping
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
-from pytkwrap.gtk3.dialog.nativedialog import GTK3NativeDialog
+from pytkwrap.gtk3.dialog.nativedialog import GTK3NativeDialogMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties
 
 
-class GTK3FileChooserNative(Gtk.FileChooserNative, GTK3NativeDialog):
-    """Wrapper for version 3.0 Gtk.FileChooserNative."""
+class GTK3FileChooserNativeMixin(GTK3NativeDialogMixin):
+    """Mixin class for GTK3FileChooserNative."""
 
     _GTK3_FILECHOOSERNATIVE_PROPERTIES = GTK3WidgetProperties(
         accept_label=None,
         cancel_label=None,
     )
 
-    def __init__(self) -> None:
-        """Initialize an instance of the GTK3FileChooserNative."""
-        Gtk.FileChooserNative.__init__(self)
-        GTK3NativeDialog.__init__(self)
+    def __init__(self, **kwargs) -> None:
+        """Initialize an instance of the GTK3FileChooserNative mixin."""
+        super().__init__(**kwargs)
 
         self.dic_properties.update(self._GTK3_FILECHOOSERNATIVE_PROPERTIES)
 
@@ -45,3 +44,30 @@ class GTK3FileChooserNative(Gtk.FileChooserNative, GTK3NativeDialog):
 
         self.set_accept_label(self.dic_properties["accept_label"])
         self.set_cancel_label(self.dic_properties["cancel_label"])
+
+
+class GTK3FileChooserNative(Gtk.FileChooserNative, GTK3FileChooserNativeMixin):
+    """Wrapper for version 3.0 Gtk.FileChooserNative."""
+
+    def __init__(
+        self,
+        title: str | None = None,
+        action: Gtk.FileChooserAction = Gtk.FileChooserAction.OPEN,
+        accept_label: str | None = None,
+        cancel_label: str | None = None,
+    ) -> None:
+        """Initialize an instance of the GTK3FileChooserNative."""
+        Gtk.FileChooserNative.__init__(
+            self,
+            title=title,
+            action=action,
+            accept_label=accept_label,
+            cancel_label=cancel_label,
+        )
+        GTK3FileChooserNativeMixin.__init__(self)
+
+        self.dic_properties["accept_label"] = accept_label
+        self.dic_properties["cancel_label"] = cancel_label
+        self.dic_properties["title"] = title
+
+        self.do_set_properties(self.dic_properties)
--- src/pytkwrap/gtk3/file/filechoosernative.pyi
+++ src/pytkwrap/gtk3/file/filechoosernative.pyi
@@ -6,12 +6,23 @@ from _typeshed import Incomplete
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.dialog.nativedialog import GTK3NativeDialog as GTK3NativeDialog
+from pytkwrap.gtk3.dialog.nativedialog import (
+    GTK3NativeDialogMixin as GTK3NativeDialogMixin,
+)
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3FileChooserNative(Gtk.FileChooserNative, GTK3NativeDialog):
+class GTK3FileChooserNativeMixin(GTK3NativeDialogMixin):
     _GTK3_FILECHOOSERNATIVE_PROPERTIES: Incomplete
-    def __init__(self) -> None: ...
+    def __init__(self, **kwargs) -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
+    ) -> None: ...
+
+class GTK3FileChooserNative(Gtk.FileChooserNative, GTK3FileChooserNativeMixin):
+    def __init__(
+        self,
+        title: str | None = None,
+        action: Gtk.FileChooserAction = ...,
+        accept_label: str | None = None,
+        cancel_label: str | None = None,
     ) -> None: ...
