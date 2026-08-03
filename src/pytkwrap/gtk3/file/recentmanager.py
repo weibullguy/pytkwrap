312c226821fbc670eb1ef17f66cad44090505e62
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-03 10:18:45 -0400

    refactor: create pure python mixin for GTK3RecentManager

--- src/pytkwrap/gtk3/file/recentmanager.py
+++ src/pytkwrap/gtk3/file/recentmanager.py
@@ -4,25 +4,32 @@
 .. copyright:: Since 2007, all rights reserved.
 """
 
-# Standard Library Imports
-
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin
 
 
-class GTK3RecentManager(Gtk.RecentManager, GTK3GObjectMixin):
-    """Wrapper for version 3.0 Gtk.RecentManager."""
+class GTK3RecentManagerMixin(GTK3GObjectMixin):
+    """Mixin class for GTK3RecentManager."""
 
     _GTK3_RECENTMANAGER_SIGNALS = [
         "changed",
     ]
 
-    def __init__(self) -> None:
-        """Initialize an instance of the GTK3RecentManager."""
-        Gtk.RecentManager.__init__(self)
-        GTK3GObjectMixin.__init__(self)
+    def __init__(self, **kwargs) -> None:
+        """Initialize an instance of the GTK3RecentManager mixin."""
+        super().__init__(**kwargs)
 
+        # Initialize public instance attributes.
         self.dic_handler_id.update(
             {_signal: -1 for _signal in self._GTK3_RECENTMANAGER_SIGNALS}
         )
+
+
+class GTK3RecentManager(Gtk.RecentManager, GTK3RecentManagerMixin):
+    """Wrapper for version 3.0 Gtk.RecentManager."""
+
+    def __init__(self) -> None:
+        """Initialize an instance of the GTK3RecentManager."""
+        Gtk.RecentManager.__init__(self)
+        GTK3RecentManagerMixin.__init__(self)
