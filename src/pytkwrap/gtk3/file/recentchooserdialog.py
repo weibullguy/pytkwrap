773c2d12f13710c99056aed22ea258a965d9a868
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-03 10:13:21 -0400

    refactor: create pure python mixin for GTK3RecentChooserDialog

--- src/pytkwrap/gtk3/file/recentchooserdialog.py
+++ src/pytkwrap/gtk3/file/recentchooserdialog.py
@@ -6,13 +6,17 @@
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
-from pytkwrap.gtk3.dialog.dialog import GTK3Dialog
+from pytkwrap.gtk3.dialog.dialog import GTK3DialogMixin
 
 
-class GTK3RecentChooserDialog(Gtk.RecentChooserDialog, GTK3Dialog):
+class GTK3RecentChooserDialogMixin(GTK3DialogMixin):
+    """Mixin class for GTK3RecentChooserDialog."""
+
+
+class GTK3RecentChooserDialog(Gtk.RecentChooserDialog, GTK3RecentChooserDialogMixin):
     """Wrapper for version 3.0 Gtk.RecentChooserDialog."""
 
     def __init__(self) -> None:
         """Initialize an instance of the GTK3RecentChooserDialog."""
         Gtk.RecentChooserDialog.__init__(self)
-        GTK3Dialog.__init__(self)
+        GTK3RecentChooserDialogMixin.__init__(self)
--- src/pytkwrap/gtk3/file/recentchooserdialog.pyi
+++ src/pytkwrap/gtk3/file/recentchooserdialog.pyi
@@ -1,6 +1,8 @@
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.dialog.dialog import GTK3Dialog as GTK3Dialog
+from pytkwrap.gtk3.dialog.dialog import GTK3DialogMixin as GTK3DialogMixin
 
-class GTK3RecentChooserDialog(Gtk.RecentChooserDialog, GTK3Dialog):
+class GTK3RecentChooserDialogMixin(GTK3DialogMixin): ...
+
+class GTK3RecentChooserDialog(Gtk.RecentChooserDialog, GTK3RecentChooserDialogMixin):
     def __init__(self) -> None: ...
