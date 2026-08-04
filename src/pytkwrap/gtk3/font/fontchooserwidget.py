7714816b601af6c58e25bc2f7e28533f6d2aa040
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-03 23:26:45 -0400

    refactor: create pure python mixin for GTK3FontChooserWidget

--- src/pytkwrap/gtk3/font/fontchooserwidget.py
+++ src/pytkwrap/gtk3/font/fontchooserwidget.py
@@ -6,13 +6,13 @@
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
-from pytkwrap.gtk3.container.box import GTK3Box
+from pytkwrap.gtk3.container.box import GTK3BoxMixin
 
 
-class GTK3FontChooserWidget(Gtk.FontChooserWidget, GTK3Box):
+class GTK3FontChooserWidget(Gtk.FontChooserWidget, GTK3BoxMixin):
     """Wrapper for version 3.0 Gtk.FontChooserWidget."""
 
     def __init__(self) -> None:
         """Initialize an instance of the GTK3FontChooserWidget."""
         Gtk.FontChooserWidget.__init__(self)
-        GTK3Box.__init__(self)
+        GTK3BoxMixin.__init__(self)
--- src/pytkwrap/gtk3/font/fontchooserwidget.pyi
+++ src/pytkwrap/gtk3/font/fontchooserwidget.pyi
@@ -1,6 +1,6 @@
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.container.box import GTK3Box as GTK3Box
+from pytkwrap.gtk3.container.box import GTK3BoxMixin as GTK3BoxMixin
 
-class GTK3FontChooserWidget(Gtk.FontChooserWidget, GTK3Box):
+class GTK3FontChooserWidget(Gtk.FontChooserWidget, GTK3BoxMixin):
     def __init__(self) -> None: ...
