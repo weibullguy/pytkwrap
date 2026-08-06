723c50fca30bc1d09ddc7aa63c6523f421712edc
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-06 08:12:41 -0400

    refactor: create pure python mixin for GTK3Layout

--- src/pytkwrap/gtk3/layout/layout.py
+++ src/pytkwrap/gtk3/layout/layout.py
@@ -9,23 +9,23 @@ from collections.abc import Mapping
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
-from pytkwrap.gtk3.container.container import GTK3Container
+from pytkwrap.gtk3.container.container import GTK3ContainerMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties
 
 
-class GTK3Layout(Gtk.Layout, GTK3Container):
-    """Wrapper for version 3.0 Gtk.Layout."""
+class GTK3LayoutMixin(GTK3ContainerMixin):
+    """Mixin class for GTK3Layout."""
 
     _GTK3_LAYOUT_PROPERTIES = GTK3WidgetProperties(
         height=100,
         width=100,
     )
 
-    def __init__(self) -> None:
-        """Initialize an instance of the GTK3Layout."""
-        Gtk.Layout.__init__(self)
-        GTK3Container.__init__(self)
+    def __init__(self, **kwargs) -> None:
+        """Initialize an instance of the GTK3Layout mixin."""
+        super().__init__(**kwargs)
 
+        # Initialize public instance attributes.
         self.dic_properties.update(self._GTK3_LAYOUT_PROPERTIES)
 
     def do_set_properties(
@@ -44,3 +44,12 @@ class GTK3Layout(Gtk.Layout, GTK3Container):
         super().do_set_properties(properties)
 
         self.set_size(self.dic_properties["width"], self.dic_properties["height"])
+
+
+class GTK3Layout(Gtk.Layout, GTK3LayoutMixin):
+    """Wrapper for version 3.0 Gtk.Layout."""
+
+    def __init__(self) -> None:
+        """Initialize an instance of the GTK3Layout."""
+        Gtk.Layout.__init__(self)
+        GTK3LayoutMixin.__init__(self)
--- src/pytkwrap/gtk3/layout/layout.pyi
+++ src/pytkwrap/gtk3/layout/layout.pyi
@@ -6,12 +6,15 @@ from _typeshed import Incomplete
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.container.container import GTK3Container as GTK3Container
+from pytkwrap.gtk3.container.container import GTK3ContainerMixin as GTK3ContainerMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3Layout(Gtk.Layout, GTK3Container):
+class GTK3LayoutMixin(GTK3ContainerMixin):
     _GTK3_LAYOUT_PROPERTIES: Incomplete
-    def __init__(self) -> None: ...
+    def __init__(self, **kwargs) -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+
+class GTK3Layout(Gtk.Layout, GTK3LayoutMixin):
+    def __init__(self) -> None: ...
