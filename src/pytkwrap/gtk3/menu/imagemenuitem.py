f4059fa1034359ef6d88819be7153339bdd40464
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-06 14:26:21 -0400

    refactor: create pure python mixin for GTK3ImageMenuItem

--- src/pytkwrap/gtk3/menu/imagemenuitem.py
+++ src/pytkwrap/gtk3/menu/imagemenuitem.py
@@ -6,13 +6,17 @@
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
-from pytkwrap.gtk3.menu.menuitem import GTK3MenuItem
+from pytkwrap.gtk3.menu.menuitem import GTK3MenuItemMixin
 
 
-class GTK3ImageMenuItem(Gtk.ImageMenuItem, GTK3MenuItem):
+class GTK3ImageMenuItemMixin(GTK3MenuItemMixin):
+    """Mixin class for GTK3ImageMenuItem."""
+
+
+class GTK3ImageMenuItem(Gtk.ImageMenuItem, GTK3ImageMenuItemMixin):
     """Wrapper for version 3.0 Gtk.ImageMenuItem."""
 
     def __init__(self) -> None:
         """Initialize an instance of the GTK3ImageMenuItem."""
         Gtk.ImageMenuItem.__init__(self)
-        GTK3MenuItem.__init__(self)
+        GTK3ImageMenuItemMixin.__init__(self)
--- src/pytkwrap/gtk3/menu/imagemenuitem.pyi
+++ src/pytkwrap/gtk3/menu/imagemenuitem.pyi
@@ -1,6 +1,8 @@
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.menu.menuitem import GTK3MenuItem as GTK3MenuItem
+from pytkwrap.gtk3.menu.menuitem import GTK3MenuItemMixin as GTK3MenuItemMixin
 
-class GTK3ImageMenuItem(Gtk.ImageMenuItem, GTK3MenuItem):
+class GTK3ImageMenuItemMixin(GTK3MenuItemMixin): ...
+
+class GTK3ImageMenuItem(Gtk.ImageMenuItem, GTK3ImageMenuItemMixin):
     def __init__(self) -> None: ...
