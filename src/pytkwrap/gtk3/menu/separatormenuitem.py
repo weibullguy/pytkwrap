6ca5235a3839c7c822a953d3eef37fbc095d90b7
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-06 21:21:32 -0400

    refactor: create pure python mixin for GTK3SeparatorMenuItem

--- src/pytkwrap/gtk3/menu/separatormenuitem.py
+++ src/pytkwrap/gtk3/menu/separatormenuitem.py
@@ -6,13 +6,17 @@
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
-from pytkwrap.gtk3.menu.menuitem import GTK3MenuItem
+from pytkwrap.gtk3.menu.menuitem import GTK3MenuItemMixin
 
 
-class GTK3SeparatorMenuItem(Gtk.SeparatorMenuItem, GTK3MenuItem):
+class GTK3SeparatorMenuItemMixin(GTK3MenuItemMixin):
+    """Mixin class for GTK3SeparatorMenuItem."""
+
+
+class GTK3SeparatorMenuItem(Gtk.SeparatorMenuItem, GTK3SeparatorMenuItemMixin):
     """Wrapper for version 3.0 Gtk.SeparatorMenuItem."""
 
     def __init__(self) -> None:
         """Initialize an instance of the GTK3SeparatorMenuItem."""
         Gtk.SeparatorMenuItem.__init__(self)
-        GTK3MenuItem.__init__(self)
+        GTK3SeparatorMenuItemMixin.__init__(self)
--- src/pytkwrap/gtk3/menu/separatormenuitem.pyi
+++ src/pytkwrap/gtk3/menu/separatormenuitem.pyi
@@ -1,6 +1,8 @@
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.menu.menuitem import GTK3MenuItem as GTK3MenuItem
+from pytkwrap.gtk3.menu.menuitem import GTK3MenuItemMixin as GTK3MenuItemMixin
 
-class GTK3SeparatorMenuItem(Gtk.SeparatorMenuItem, GTK3MenuItem):
+class GTK3SeparatorMenuItemMixin(GTK3MenuItemMixin): ...
+
+class GTK3SeparatorMenuItem(Gtk.SeparatorMenuItem, GTK3SeparatorMenuItemMixin):
     def __init__(self) -> None: ...
