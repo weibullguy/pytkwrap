b4ae9202787d93dffc5a5dedc8b4ae368a9e428c
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-06 22:00:17 -0400

    chore: remove deprecated GTK3TearOffMenuItem

--- src/pytkwrap/gtk3/menu/tearoffmenuitem.pyi
+++
@@ -1,8 +0,0 @@
-# pytkwrap Package Imports
-from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.menu.menuitem import GTK3MenuItemMixin as GTK3MenuItemMixin
-
-class GTK3TearoffMenuItemMixin(GTK3MenuItemMixin): ...
-
-class GTK3TearoffMenuItem(Gtk.TearoffMenuItem, GTK3TearoffMenuItemMixin):
-    def __init__(self) -> None: ...
