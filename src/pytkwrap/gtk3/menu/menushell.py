4306de22c8befa2b1c3593c358edafe73f3f1f3e
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-06 20:30:58 -0400

    refactor: create pure python mixin for GTK3MenuShell

--- src/pytkwrap/gtk3/menu/menushell.py
+++ src/pytkwrap/gtk3/menu/menushell.py
@@ -9,12 +9,12 @@ from collections.abc import Mapping
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
-from pytkwrap.gtk3.container.container import GTK3Container
+from pytkwrap.gtk3.container.container import GTK3ContainerMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties
 
 
-class GTK3MenuShell(Gtk.MenuShell, GTK3Container):
-    """Wrapper for version 3.0 Gtk.MenuShell."""
+class GTK3MenuShellMixin(GTK3ContainerMixin):
+    """Mixin class for GTK3MenuShell."""
 
     _GTK3_MENUSHELL_PROPERTIES = GTK3WidgetProperties(
         take_focus=True,
@@ -30,11 +30,11 @@ class GTK3MenuShell(Gtk.MenuShell, GTK3Container):
         "selection-done",
     ]
 
-    def __init__(self) -> None:
-        """Initialize an instance of the GTK3MenuShell."""
-        Gtk.MenuShell.__init__(self)
-        GTK3Container.__init__(self)
+    def __init__(self, **kwargs) -> None:
+        """Initialize an instance of the GTK3MenuShell mixin."""
+        super().__init__(**kwargs)
 
+        # initialize public instance attributes
         self.dic_handler_id.update(
             {_signal: -1 for _signal in self._GTK3_MENUSHELL_SIGNALS}
         )
@@ -56,3 +56,12 @@ class GTK3MenuShell(Gtk.MenuShell, GTK3Container):
         super().do_set_properties(properties)
 
         self.set_take_focus(self.dic_properties["take_focus"])
+
+
+class GTK3MenuShell(Gtk.MenuShell, GTK3MenuShellMixin):
+    """Wrapper for version 3.0 Gtk.MenuShell."""
+
+    def __init__(self) -> None:
+        """Initialize an instance of the GTK3MenuShell."""
+        Gtk.MenuShell.__init__(self)
+        GTK3MenuShellMixin.__init__(self)
