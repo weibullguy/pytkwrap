75f8c6975239b414729acb76c1e44ffeca5c66a5
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-07 08:46:33 -0400

    refactor: create pure python mixin for GTK3CssProvider

--- src/pytkwrap/gtk3/style/cssprovider.py
+++ src/pytkwrap/gtk3/style/cssprovider.py
@@ -9,18 +9,27 @@ from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin
 
 
-class GTK3CssProvider(Gtk.CssProvider, GTK3GObjectMixin):
-    """Wrapper for version 3.0 Gtk.CssProvider."""
+class GTK3CssProviderMixin(GTK3GObjectMixin):
+    """Mixin class for GTK3CssProvider."""
 
     _GTK3_CSSPROVIDER_SIGNALS = [
         "parsing-error",
     ]
 
-    def __init__(self) -> None:
-        """Initialize an instance of the GTK3CssProvider."""
-        Gtk.CssProvider.__init__(self)
-        GTK3GObjectMixin.__init__(self)
+    def __init__(self, **kwargs) -> None:
+        """Initialize an instance of the GTK3CssProvider mixin."""
+        super().__init__(**kwargs)
 
+        # Initialize public instance attributes.
         self.dic_handler_id.update(
             {_signal: -1 for _signal in self._GTK3_CSSPROVIDER_SIGNALS}
         )
+
+
+class GTK3CssProvider(Gtk.CssProvider, GTK3CssProviderMixin):
+    """Wrapper for version 3.0 Gtk.CssProvider."""
+
+    def __init__(self) -> None:
+        """Initialize an instance of the GTK3CssProvider."""
+        Gtk.CssProvider.__init__(self)
+        GTK3CssProviderMixin.__init__(self)
--- src/pytkwrap/gtk3/style/cssprovider.pyi
+++ src/pytkwrap/gtk3/style/cssprovider.pyi
@@ -5,6 +5,9 @@ from _typeshed import Incomplete
 from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
 
-class GTK3CssProvider(Gtk.CssProvider, GTK3GObjectMixin):
+class GTK3CssProviderMixin(GTK3GObjectMixin):
     _GTK3_CSSPROVIDER_SIGNALS: Incomplete
+    def __init__(self, **kwargs) -> None: ...
+
+class GTK3CssProvider(Gtk.CssProvider, GTK3CssProviderMixin):
     def __init__(self) -> None: ...
