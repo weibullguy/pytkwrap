187827a4fca1ec9d5dd9eea0e7b70ef046968811
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-07 16:50:42 -0400

    refactor: create pure python mixin for GTK3TextMark

--- src/pytkwrap/gtk3/text/textmark.py
+++ src/pytkwrap/gtk3/text/textmark.py
@@ -4,20 +4,28 @@
 .. copyright:: Since 2007, all rights reserved.
 """
 
-# Standard Library Imports
-
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin, GTK3WidgetProperties
 
 
-class GTK3TextMark(Gtk.TextMark, GTK3GObjectMixin):
-    """Wrapper for version 3.0 Gtk.TextMark."""
+class GTK3TextMarkMixin(GTK3GObjectMixin):
+    """Mixin class for GTK3TextMark."""
 
     _GTK3_TEXTMARK_PROPERTIES = GTK3WidgetProperties(
         left_gravity=False,
         name=None,
     )
+
+    def __init__(self, **kwargs) -> None:
+        """Initialize an instance of the GTK3TextMark."""
+        super().__init__(**kwargs)
+
+        self.dic_properties = dict(self._GTK3_TEXTMARK_PROPERTIES)
+
+
+class GTK3TextMark(Gtk.TextMark, GTK3TextMarkMixin):
+    """Wrapper for version 3.0 Gtk.TextMark."""
 
     def __init__(self, name: str | None = None, left_gravity: bool = False) -> None:
         """Initialize an instance of the GTK3TextMark.
@@ -30,9 +38,7 @@ class GTK3TextMark(Gtk.TextMark, GTK3GObjectMixin):
             Whether the text mark should have left-gravity.
         """
         Gtk.TextMark.__init__(self, name=name, left_gravity=left_gravity)
-        GTK3GObjectMixin.__init__(self)
-
-        self.dic_properties = dict(self._GTK3_TEXTMARK_PROPERTIES)
+        GTK3TextMarkMixin.__init__(self)
 
         self.dic_properties["left_gravity"] = left_gravity
         self.dic_properties["name"] = name
--- src/pytkwrap/gtk3/text/textmark.pyi
+++ src/pytkwrap/gtk3/text/textmark.pyi
@@ -6,7 +6,10 @@ from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3TextMark(Gtk.TextMark, GTK3GObjectMixin):
+class GTK3TextMarkMixin(GTK3GObjectMixin):
     _GTK3_TEXTMARK_PROPERTIES: Incomplete
     dic_properties: Incomplete
+    def __init__(self, **kwargs) -> None: ...
+
+class GTK3TextMark(Gtk.TextMark, GTK3TextMarkMixin):
     def __init__(self, name: str | None = None, left_gravity: bool = False) -> None: ...
