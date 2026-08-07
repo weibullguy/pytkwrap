4fdeb0a08c2d421c2d5487133448c15bb471aa3f
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-07 17:39:32 -0400

    refactor: create pure python mixin for GTK3ToggleToolButton

--- src/pytkwrap/gtk3/tool/toggletoolbutton.py
+++ src/pytkwrap/gtk3/tool/toggletoolbutton.py
@@ -10,22 +10,22 @@ from collections.abc import Mapping
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties
-from pytkwrap.gtk3.tool.toolbutton import GTK3ToolButton
+from pytkwrap.gtk3.tool.toolbutton import GTK3ToolButtonMixin
 
 
-class GTK3ToggleToolButton(Gtk.ToggleToolButton, GTK3ToolButton):
-    """Wrapper for version 3.0 Gtk.ToggleToolButton."""
+class GTK3ToggleToolButtonMixin(GTK3ToolButtonMixin):
+    """Mixin class for GTK3ToggleToolButton."""
 
     _GTK3_TOGGLETOOLBUTTON_PROPERTIES = GTK3WidgetProperties(
         active=False,
     )
     _GTK3_TOGGLETOOLBUTTON_SIGNALS = ["toggled"]
 
-    def __init__(self) -> None:
-        """Initialize an instance of the GTK3ToggleToolButton."""
-        Gtk.ToggleToolButton.__init__(self)
-        GTK3ToolButton.__init__(self)
+    def __init__(self, **kwargs) -> None:
+        """Initialize an instance of the GTK3ToggleToolButton mixin."""
+        super().__init__(**kwargs)
 
+        # Initialize public instance attributes.
         self.dic_handler_id.update(
             {_signal: -1 for _signal in self._GTK3_TOGGLETOOLBUTTON_SIGNALS}
         )
@@ -47,3 +47,12 @@ class GTK3ToggleToolButton(Gtk.ToggleToolButton, GTK3ToolButton):
         super().do_set_properties(properties)
 
         self.set_active(self.dic_properties["active"])
+
+
+class GTK3ToggleToolButton(Gtk.ToggleToolButton, GTK3ToggleToolButtonMixin):
+    """Wrapper for version 3.0 Gtk.ToggleToolButton."""
+
+    def __init__(self) -> None:
+        """Initialize an instance of the GTK3ToggleToolButton."""
+        Gtk.ToggleToolButton.__init__(self)
+        GTK3ToggleToolButtonMixin.__init__(self)
--- src/pytkwrap/gtk3/tool/toggletoolbutton.pyi
+++ src/pytkwrap/gtk3/tool/toggletoolbutton.pyi
@@ -7,12 +7,15 @@ from _typeshed import Incomplete
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
-from pytkwrap.gtk3.tool.toolbutton import GTK3ToolButton as GTK3ToolButton
+from pytkwrap.gtk3.tool.toolbutton import GTK3ToolButtonMixin as GTK3ToolButtonMixin
 
-class GTK3ToggleToolButton(Gtk.ToggleToolButton, GTK3ToolButton):
+class GTK3ToggleToolButtonMixin(GTK3ToolButtonMixin):
     _GTK3_TOGGLETOOLBUTTON_PROPERTIES: Incomplete
     _GTK3_TOGGLETOOLBUTTON_SIGNALS: Incomplete
-    def __init__(self) -> None: ...
+    def __init__(self, **kwargs) -> None: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+
+class GTK3ToggleToolButton(Gtk.ToggleToolButton, GTK3ToggleToolButtonMixin):
+    def __init__(self) -> None: ...
