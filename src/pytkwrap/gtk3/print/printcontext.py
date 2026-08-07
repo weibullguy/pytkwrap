e44feca6be3a2cc5fb9a634ab4c79389b2ecab52
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-06 22:10:37 -0400

    refactor: create pure python mixin for GTK3PrintContext

--- src/pytkwrap/gtk3/print/printcontext.py
+++ src/pytkwrap/gtk3/print/printcontext.py
@@ -9,10 +9,14 @@ from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin
 
 
-class GTK3PrintContext(Gtk.PrintContext, GTK3GObjectMixin):
+class GTK3PrintContextMixin(GTK3GObjectMixin):
+    """Mixin class for GTK3PrintContext."""
+
+
+class GTK3PrintContext(Gtk.PrintContext, GTK3PrintContextMixin):
     """Wrapper for version 3.0 Gtk.PrintContext."""
 
     def __init__(self) -> None:
         """Initialize an instance of the GTK3PrintContext."""
         Gtk.PrintContext.__init__(self)
-        GTK3GObjectMixin.__init__(self)
+        GTK3PrintContextMixin.__init__(self)
--- src/pytkwrap/gtk3/print/printcontext.pyi
+++ src/pytkwrap/gtk3/print/printcontext.pyi
@@ -2,5 +2,7 @@
 from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
 
-class GTK3PrintContext(Gtk.PrintContext, GTK3GObjectMixin):
+class GTK3PrintContextMixin(GTK3GObjectMixin): ...
+
+class GTK3PrintContext(Gtk.PrintContext, GTK3PrintContextMixin):
     def __init__(self) -> None: ...
