37a525d88f9896a8456e58ddb96a496947650850
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-06 21:36:23 -0400

    chore: remove deprecated GTK3ImageMenuItem

--- src/pytkwrap/gtk3/menu/imagemenuitem.pyi
+++
@@ -1,8 +0,0 @@
-# pytkwrap Package Imports
-from pytkwrap.gtk3._libs import Gtk as Gtk
-from pytkwrap.gtk3.menu.menuitem import GTK3MenuItemMixin as GTK3MenuItemMixin
-
-class GTK3ImageMenuItemMixin(GTK3MenuItemMixin): ...
-
-class GTK3ImageMenuItem(Gtk.ImageMenuItem, GTK3ImageMenuItemMixin):
-    def __init__(self) -> None: ...
