680f0ff800a43fb425633db964a1b0e096592e96
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-09 12:37:29 -0400

    refactor: create pure python mixin for GTK3TreeModelSort

--- src/pytkwrap/gtk3/treeview/treemodelsort.py
+++ src/pytkwrap/gtk3/treeview/treemodelsort.py
@@ -9,12 +9,23 @@ from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin, GTK3WidgetProperties
 
 
-class GTK3TreeModelSort(Gtk.TreeModelSort, GTK3GObjectMixin):
-    """Wrapper for version 3.0 Gtk.TreeModelSort."""
+class GTK3TreeModelSortMixin(GTK3GObjectMixin):
+    """Mixin class for GTK3TreeModelSort."""
 
     _GTK3_TREEMODELSORT_PROPERTIES = GTK3WidgetProperties(
         model=None,
     )
+
+    def __init__(self, **kwargs) -> None:
+        """Initialize an instance of the GTK3TreeModelSort mixin."""
+        super().__init__(**kwargs)
+
+        # Initialize public instance attributes.
+        self.dic_properties.update(self._GTK3_TREEMODELSORT_PROPERTIES)
+
+
+class GTK3TreeModelSort(Gtk.TreeModelSort, GTK3TreeModelSortMixin):
+    """Wrapper for version 3.0 Gtk.TreeModelSort."""
 
     def __init__(
         self,
@@ -22,7 +33,6 @@ class GTK3TreeModelSort(Gtk.TreeModelSort, GTK3GObjectMixin):
     ) -> None:
         """Initialize an instance of the GTK3TreeModelSort."""
         Gtk.TreeModelSort.__init__(self, model=model)
-        GTK3GObjectMixin.__init__(self)
+        GTK3TreeModelSortMixin.__init__(self)
 
-        self.dic_properties.update(self._GTK3_TREEMODELSORT_PROPERTIES)
         self.dic_properties["model"] = model
--- src/pytkwrap/gtk3/treeview/treemodelsort.pyi
+++ src/pytkwrap/gtk3/treeview/treemodelsort.pyi
@@ -6,6 +6,9 @@ from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 
-class GTK3TreeModelSort(Gtk.TreeModelSort, GTK3GObjectMixin):
+class GTK3TreeModelSortMixin(GTK3GObjectMixin):
     _GTK3_TREEMODELSORT_PROPERTIES: Incomplete
+    def __init__(self, **kwargs) -> None: ...
+
+class GTK3TreeModelSort(Gtk.TreeModelSort, GTK3TreeModelSortMixin):
     def __init__(self, model: Gtk.TreeModel | None) -> None: ...
