f1ed7df00f51063bf55564e18a07e8c1767a387f
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-09 11:57:48 -0400

    refactor: create pure python mixin for GTK3CellRendererSpin

--- src/pytkwrap/gtk3/treeview/cellrendererspin.py
+++ src/pytkwrap/gtk3/treeview/cellrendererspin.py
@@ -6,15 +6,16 @@
 
 # Standard Library Imports
 from collections.abc import Mapping
+from datetime import date
 
 # pytkwrap Package Imports
 from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties
-from pytkwrap.gtk3.treeview.cellrenderertext import GTK3CellRendererText
+from pytkwrap.gtk3.treeview.cellrenderertext import GTK3CellRendererTextMixin
 
 
-class GTK3CellRendererSpin(Gtk.CellRendererSpin, GTK3CellRendererText):
-    """Wrapper for version 3.0 Gtk.CellRendererSpin."""
+class GTK3CellRendererSpinMixin(GTK3CellRendererTextMixin):
+    """Mixin class for GTK3CellRendererSpin."""
 
     _GTK3_CELLRENDERERSPIN_PROPERTIES = GTK3WidgetProperties(
         adjustment=None,
@@ -22,12 +23,21 @@ class GTK3CellRendererSpin(Gtk.CellRendererSpin, GTK3CellRendererText):
         digits=0,
     )
 
-    def __init__(self) -> None:
-        """Initialize an instance of the GTK3CellRenderer."""
-        Gtk.CellRendererSpin.__init__(self)
-        GTK3CellRendererText.__init__(self)
+    def __init__(self, **kwargs) -> None:
+        """Initialize an instance of the GTK3CellRendererSpin mixin."""
+        super().__init__(**kwargs)
 
+        # Initialize public instance attributes.
         self.dic_properties.update(self._GTK3_CELLRENDERERSPIN_PROPERTIES)
+
+    def do_get_value(self) -> float:
+        """Get the value of the GTK3CellRendererSpin.
+
+        Returns
+        -------
+        The value of the GTK3CellRendererSpin.  A float representing the value.
+        """
+        return self.get_property("adjustment").get_value()
 
     def do_set_properties(
         self,
@@ -52,3 +62,26 @@ class GTK3CellRendererSpin(Gtk.CellRendererSpin, GTK3CellRendererText):
             self.set_property(
                 _property.replace("_", "-"), self.dic_properties[_property]
             )
+
+    def do_set_value(
+        self, value: bool | date | float | int | object | str | tuple | None
+    ) -> None:
+        """Set the value of the GTK3CellRendererSpin.
+
+        Parameters
+        ----------
+        value : bool | date | float | int | object | str | tuple | None
+            The number to set the value of the GTK3CellRendererSpin.
+        """
+        if not isinstance(value, (float, int, str)):
+            super().do_set_value(value)
+        self.get_property("adjustment").set_value(float(value))  # type: ignore[arg-type] # ty: ignore[invalid-argument-type] # pylint: disable=line-too-long
+
+
+class GTK3CellRendererSpin(Gtk.CellRendererSpin, GTK3CellRendererSpinMixin):
+    """Wrapper for version 3.0 Gtk.CellRendererSpin."""
+
+    def __init__(self) -> None:
+        """Initialize an instance of the GTK3CellRenderer."""
+        Gtk.CellRendererSpin.__init__(self)
+        GTK3CellRendererSpinMixin.__init__(self)
--- src/pytkwrap/gtk3/treeview/cellrendererspin.pyi
+++ src/pytkwrap/gtk3/treeview/cellrendererspin.pyi
@@ -1,5 +1,6 @@
 # Standard Library Imports
 from collections.abc import Mapping
+from datetime import date
 
 # Third Party Imports
 from _typeshed import Incomplete
@@ -8,12 +9,19 @@ from _typeshed import Incomplete
 from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties as GTK3WidgetProperties
 from pytkwrap.gtk3.treeview.cellrenderertext import (
-    GTK3CellRendererText as GTK3CellRendererText,
+    GTK3CellRendererTextMixin as GTK3CellRendererTextMixin,
 )
 
-class GTK3CellRendererSpin(Gtk.CellRendererSpin, GTK3CellRendererText):
+class GTK3CellRendererSpinMixin(GTK3CellRendererTextMixin):
     _GTK3_CELLRENDERERSPIN_PROPERTIES: Incomplete
-    def __init__(self) -> None: ...
+    def __init__(self, **kwargs) -> None: ...
+    def do_get_value(self) -> float: ...
     def do_set_properties(
         self, properties: Mapping[str, object] | list[list | tuple]
     ) -> None: ...
+    def do_set_value(
+        self, value: bool | date | float | int | object | str | tuple | None
+    ) -> None: ...
+
+class GTK3CellRendererSpin(Gtk.CellRendererSpin, GTK3CellRendererSpinMixin):
+    def __init__(self) -> None: ...
