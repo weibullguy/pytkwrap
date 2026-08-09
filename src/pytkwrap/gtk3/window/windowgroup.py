8bdb1b7342a52b60c9e3554ed19e113b993da1a1
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-09 18:52:51 -0400

    refactor: create pure python mixin for GTK3WindowGroup

--- src/pytkwrap/gtk3/window/windowgroup.py
+++ src/pytkwrap/gtk3/window/windowgroup.py
@@ -9,10 +9,14 @@ from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin
 
 
-class GTK3WindowGroup(Gtk.WindowGroup, GTK3GObjectMixin):
+class GTK3WindowGroupMixin(GTK3GObjectMixin):
+    """Mixin class for GTK3WindowGroup."""
+
+
+class GTK3WindowGroup(Gtk.WindowGroup, GTK3WindowGroupMixin):
     """Wrapper for version 3.0 Gtk.WindowGroup."""
 
     def __init__(self) -> None:
         """Initialize an instance of the GTK3WindowGroup."""
         Gtk.WindowGroup.__init__(self)
-        GTK3GObjectMixin.__init__(self)
+        GTK3WindowGroupMixin.__init__(self)
--- src/pytkwrap/gtk3/window/windowgroup.pyi
+++ src/pytkwrap/gtk3/window/windowgroup.pyi
@@ -2,5 +2,7 @@
 from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.mixins import GTK3GObjectMixin as GTK3GObjectMixin
 
-class GTK3WindowGroup(Gtk.WindowGroup, GTK3GObjectMixin):
+class GTK3WindowGroupMixin(GTK3GObjectMixin): ...
+
+class GTK3WindowGroup(Gtk.WindowGroup, GTK3WindowGroupMixin):
     def __init__(self) -> None: ...
