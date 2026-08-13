1d9d25105c8d4205cb2c8af2467b48725ef42280
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-12 21:43:16 -0400

    feat: add size argument to GTK3VolumeButton __init__ method

--- src/pytkwrap/gtk3/button/volumebutton.py
+++ src/pytkwrap/gtk3/button/volumebutton.py
@@ -14,7 +14,12 @@ from pytkwrap.gtk3.mixins import GTK3WidgetProperties
 
 
 class GTK3VolumeButtonMixin(GTK3ScaleButtonMixin):
-    """Mixin for GTK3VolumeButton."""
+    """Mixin for GTK3VolumeButton.
+
+    Notes
+    -----
+    GTK3VolumeButton passes a Gtk.Image to its callback function.
+    """
 
     # Define private class attributes.
     _DEFAULT_HEIGHT = 30
@@ -42,6 +47,7 @@ class GTK3VolumeButtonMixin(GTK3ScaleButtonMixin):
             The typed dict (preferred), non-typed dict, list of lists, or list of
             tuples with the property values to set for the GTK3VolumeButton.
         """
+        # Update the property dictionary.
         super().do_set_properties(properties)
 
         self.set_property("use-symbolic", self.dic_properties["use_symbolic"])
@@ -50,7 +56,15 @@ class GTK3VolumeButtonMixin(GTK3ScaleButtonMixin):
 class GTK3VolumeButton(Gtk.VolumeButton, GTK3VolumeButtonMixin):
     """Wrapper for version 3.0 Gtk.VolumeButton."""
 
-    def __init__(self) -> None:
-        """Initialize an instance of the GTK3VolumeButton widget."""
-        super().__init__(size=4)
+    def __init__(self, size: int = 4) -> None:
+        """Initialize an instance of the GTK3VolumeButton widget.
+
+        Parameters
+        ----------
+        size : int
+            A stock icon size.  The default is 4.
+        """
+        Gtk.VolumeButton.__init__(self, size=size)
         GTK3VolumeButtonMixin.__init__(self)
+
+        self.dic_properties["size"] = size
