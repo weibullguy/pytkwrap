d6743c741ab7bc0cd66de1f144805cff902ce579
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-16 11:03:54 -0400

    feat: add methods to add and remove recent items from the GTK3RecentManager

--- src/pytkwrap/gtk3/file/recentmanager.py
+++ src/pytkwrap/gtk3/file/recentmanager.py
@@ -10,7 +10,12 @@ from pytkwrap.gtk3.mixins import GTK3GObjectMixin
 
 
 class GTK3RecentManagerMixin(GTK3GObjectMixin):
-    """Mixin class for GTK3RecentManager."""
+    """Mixin class for GTK3RecentManager.
+
+    Notes
+    -----
+    GTK3RecentManager passes no widgets to its callback function.
+    """
 
     _GTK3_RECENTMANAGER_SIGNALS = [
         "changed",
@@ -33,3 +38,48 @@ class GTK3RecentManager(Gtk.RecentManager, GTK3RecentManagerMixin):
         """Initialize an instance of the GTK3RecentManager."""
         Gtk.RecentManager.__init__(self)
         GTK3RecentManagerMixin.__init__(self)
+
+    def do_add_recent(
+        self,
+        uri: str,
+        recent_data: Gtk.RecentData | None = None,
+    ) -> bool:
+        """Add a recent file.
+
+        Parameters
+        ----------
+        uri : str
+            A valid URI pointing to the file to add to the manager.
+        recent_data : Gtk.RecentData, optional
+            Metadata to associate with the file.
+
+        Returns
+        -------
+            True if the new item was successfully added, False otherwise.
+        """
+        if recent_data is None:
+            return self.add_item(uri)
+        return self.add_full(uri, recent_data)
+
+    def do_remove_recent(self, uri: str, remove_all: bool = False) -> bool:
+        """Remove a recent file.
+
+        Parameters
+        ----------
+        uri : str
+            The URI of the file to remove.
+        remove_all : bool, optional
+            True to remove all items, False to remove only the item with the given URI.
+
+        Returns
+        -------
+            True if the item was successfully removed, False otherwise.
+        """
+        if remove_all:
+            return bool(self.purge_items())
+        return self.remove_item(uri)
+
+    @property
+    def recent_items(self) -> list[Gtk.RecentData]:
+        """Return a list of Gtk.RecentData objects."""
+        return self.get_items()
