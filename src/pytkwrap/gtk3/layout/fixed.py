96c3a4a69a6285e1663776985363b5c05638212a
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-08-30 13:34:12 -0400

    refactor: remove unused GTK3FixedMixin

--- src/pytkwrap/gtk3/layout/fixed.py
+++ src/pytkwrap/gtk3/layout/fixed.py
@@ -9,14 +9,15 @@ from pytkwrap.gtk3._libs import Gtk
 from pytkwrap.gtk3.container.container import GTK3ContainerMixin
 
 
-class GTK3FixedMixin(GTK3ContainerMixin):
-    """Mixin class for GTK3Fixed."""
+class GTK3Fixed(Gtk.Fixed, GTK3ContainerMixin):
+    """Wrapper for version 3.0 Gtk.Fixed.
 
-
-class GTK3Fixed(Gtk.Fixed, GTK3FixedMixin):
-    """Wrapper for version 3.0 Gtk.Fixed."""
+    Notes
+    -----
+    GTK3Fixed passes no widgets to its callback function.
+    """
 
     def __init__(self) -> None:
         """Initialize an instance of the GTK3Fixed."""
         Gtk.Fixed.__init__(self)
-        GTK3FixedMixin.__init__(self)
+        GTK3ContainerMixin.__init__(self)
--- src/pytkwrap/gtk3/layout/fixed.pyi
+++ src/pytkwrap/gtk3/layout/fixed.pyi
@@ -2,7 +2,5 @@
 from pytkwrap.gtk3._libs import Gtk as Gtk
 from pytkwrap.gtk3.container.container import GTK3ContainerMixin as GTK3ContainerMixin
 
-class GTK3FixedMixin(GTK3ContainerMixin): ...
-
-class GTK3Fixed(Gtk.Fixed, GTK3FixedMixin):
+class GTK3Fixed(Gtk.Fixed, GTK3ContainerMixin):
     def __init__(self) -> None: ...
