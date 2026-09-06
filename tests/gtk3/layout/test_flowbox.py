44aed8881b612125eb76a317ad125a781719697e
Doyle Rowland <doyle.rowland@reliaqual.com>
2026-09-05 20:04:00 -0400

    test: add tests for new GTK3FlowBox methods

--- tests/gtk3/layout/test_flowbox.py
+++ tests/gtk3/layout/test_flowbox.py
@@ -10,6 +10,7 @@ import pytest
 # pytkwrap Package Imports
 # noinspection PyProtectedMember
 from pytkwrap.gtk3._libs import Gtk
+from pytkwrap.gtk3.io import GTK3Entry
 from pytkwrap.gtk3.layout import GTK3FlowBox
 from pytkwrap.gtk3.mixins import GTK3WidgetProperties
 from tests.gtk3.conftest import BaseGTK3GObjectTests
@@ -106,3 +107,78 @@ class TestGTK3FlowBox(BaseGTK3GObjectTests):
         assert dut.get_row_spacing() == 8
         assert dut.get_property("selection_mode") == Gtk.SelectionMode.BROWSE
         assert dut.get_selection_mode() == Gtk.SelectionMode.BROWSE
+
+    @pytest.mark.unit
+    def test_do_populate_flowbox(self):
+        """Should populate the GTK3FlowBox."""
+        dut = self.make_dut()
+        dut.do_populate_flowbox([(GTK3Entry(), i) for i in range(4)])
+
+        for _index in range(4):
+            assert isinstance(dut.get_child_at_index(_index).get_child(), GTK3Entry)
+
+    @pytest.mark.unit
+    def test_do_unpopulate_flowbox(self):
+        """Should unpopulate the GTK3FlowBox."""
+        _entry_1 = GTK3Entry()
+        _entry_2 = GTK3Entry()
+        _entry_3 = GTK3Entry()
+        _entry_4 = GTK3Entry()
+
+        dut = self.make_dut()
+        dut.do_populate_flowbox([(_entry_1, 0)])
+        dut.do_populate_flowbox([(_entry_2, 1)])
+        dut.do_populate_flowbox([(_entry_3, 2)])
+        dut.do_populate_flowbox([(_entry_4, 3)])
+
+        assert dut.get_child_at_index(0).get_child() == _entry_1
+        assert dut.get_child_at_index(1).get_child() == _entry_2
+        assert dut.get_child_at_index(2).get_child() == _entry_3
+        assert dut.get_child_at_index(3).get_child() == _entry_4
+
+        dut.do_unpopulate_flowbox([3, 1])
+        assert dut.get_child_at_index(0).get_child() == _entry_1
+        assert dut.get_child_at_index(1).get_child() == _entry_3
+        assert dut.get_child_at_index(2) is None
+        assert dut.get_child_at_index(3) is None
+
+    @pytest.mark.unit
+    def test_do_unpopulate_flowbox_last_two_widgets(self):
+        """Should unpopulate the GTK3FlowBox."""
+        _entry_1 = GTK3Entry()
+        _entry_2 = GTK3Entry()
+        _entry_3 = GTK3Entry()
+        _entry_4 = GTK3Entry()
+
+        dut = self.make_dut()
+        dut.do_populate_flowbox([(_entry_1, 0)])
+        dut.do_populate_flowbox([(_entry_2, 1)])
+        dut.do_populate_flowbox([(_entry_3, 2)])
+        dut.do_populate_flowbox([(_entry_4, 3)])
+
+        assert dut.get_child_at_index(0).get_child() == _entry_1
+        assert dut.get_child_at_index(1).get_child() == _entry_2
+        assert dut.get_child_at_index(2).get_child() == _entry_3
+        assert dut.get_child_at_index(3).get_child() == _entry_4
+
+        dut.do_unpopulate_flowbox([2, 2])
+        assert dut.get_child_at_index(0).get_child() == _entry_1
+        assert dut.get_child_at_index(1).get_child() == _entry_2
+        assert dut.get_child_at_index(2) is None
+        assert dut.get_child_at_index(3) is None
+
+    @pytest.mark.unit
+    def test_do_unpopulate_flowbox_no_widget(self):
+        """Should unpopulate the GTK3FlowBox."""
+        _entry_1 = GTK3Entry()
+        _entry_2 = GTK3Entry()
+
+        dut = self.make_dut()
+        dut.do_populate_flowbox([(_entry_1, 0)])
+        dut.do_populate_flowbox([(_entry_2, 1)])
+
+        assert dut.get_child_at_index(0).get_child() == _entry_1
+        assert dut.get_child_at_index(1).get_child() == _entry_2
+
+        with pytest.raises(TypeError):
+            dut.do_unpopulate_flowbox([3])
